918d332582ee2b03d474e717de20f6a5b015ed7a
bayalams <110493296+bayalams@users.noreply.github.com>
2023-07-17 22:08:31 +0000

    Mon, Jul 17, 2023, 10:08 PM +00:00

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
 name = input("what's your name? ")
 print("hello, ", end = "")
+#the "end" bit overrides the default \n at the end of every print, which means that the name will appear right after "hello"
 print(name)
 
 
