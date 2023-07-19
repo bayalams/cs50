f55ea774bfb1567e5a78ede55dc7dea262adf0a2
bayalams <110493296+bayalams@users.noreply.github.com>
2023-07-19 21:09:19 +0000

    Wed, Jul 19, 2023, 10:09 PM +01:00

---
+++ faces/faces.py
@@ -0,0 +1,13 @@
+def main():
+    s = input()
+    
+
+
+def convert(s):
+
+    for i in s:
+        if i == ":(" or i == ":)":
+            i.replace(":(", "🙁") or i. replace(":)", "🙂")
+
+    print(s)
+    return s
