4e0577da130c066ae71bcea035971a7d98299470
bayalams <110493296+bayalams@users.noreply.github.com>
2023-07-19 21:47:21 +0000

    Wed, Jul 19, 2023, 10:47 PM +01:00

--- tip/tip.py
+++ tip/tip.py
@@ -1,5 +1,6 @@
 meal_price = float(input("how much was the meal? "))
 percentage = float(input("what percentage would you like to tip? "))
+percentage = percentage/100
 
 tip = meal_price * percentage
 print(tip)
