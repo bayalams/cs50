a4b24a464b538bcfcdc2749621498f64ea60d7a7
bayalams <110493296+bayalams@users.noreply.github.com>
2023-07-20 22:37:49 +0000

    Thu, Jul 20, 2023, 11:37 PM +01:00

--- tip/tip.py
+++ tip/tip.py
@@ -1,16 +1,24 @@
+def main():
 
+    dollars = dollars_to_float(input("How much was dinner? "))
+    percentage = percentage_to_float(input("What percentage do you want to tip? "))
+    tip = dollars * percentage
+    print(f"Leave ${tip:.2f}")
 
+def dollars_to_float(d):
 
+    d = input("insert ammount: ")
+    d = float(d.replace("$", ""))
+    print(d)
+    return d
 
-d = input("insert ammount: ")
-d = float(d.replace("$", ""))
-print(d)
+def percentage_to_float(p):
 
-p = input("insert percentage: ")
-p = float(p.replace("%", ""))
-p = (p/100)
-print(p)
-
+    p = input("insert percentage: ")
+    p = float(p.replace("%", ""))
+    p = (p/100)
+    print(p)
+    return p
 
 
 
