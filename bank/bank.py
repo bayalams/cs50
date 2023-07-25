2085bafbad95a2c818117eab9a5cdbef53a54c98
bayalams <110493296+bayalams@users.noreply.github.com>
2023-07-25 21:55:52 +0000

    Tue, Jul 25, 2023, 10:55 PM +01:00

--- bank/bank.py
+++ bank/bank.py
@@ -1,8 +1,8 @@
-# greeting = input("Greeting: ")
-# greeting = greeting.lower()
-# print(greeting[0:5])
+greeting = input("Greeting: ")
+greeting = greeting.lower()
+print(greeting[0:5])
 
-# if greeting[0:5] == "hello":
-#     print("$0")
-# else:
-#     print("nope")
+if greeting[0:5] == "hello":
+    print("$0")
+else:
+    print("nope")
