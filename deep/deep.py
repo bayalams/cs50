aa7dafa18bb6d10d78e5fd29c43296940344d6bf
bayalams <110493296+bayalams@users.noreply.github.com>
2023-07-25 20:50:12 +0000

    Tue, Jul 25, 2023, 9:50 PM +01:00

---
+++ deep/deep.py
@@ -0,0 +1,6 @@
+s = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ")
+
+if s == "42" or "Forty Two" or "Forty-Two" or "forty two" or "forty-two":
+    print("Yes")
+else:
+    print("No")
