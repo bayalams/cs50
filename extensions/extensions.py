74730c7f8266966728fb21428dc01970534c6051
bayalams <110493296+bayalams@users.noreply.github.com>
2023-07-25 22:20:52 +0000

    Tue, Jul 25, 2023, 11:20 PM +01:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -1,5 +1,17 @@
 file_name = input("File Name: ")
 file_name.lower().strip()
 
-if file_name.endswith(".jpeg"):
-    print("tomatio")
+if file_name.endswith(".gif"):
+    print("image/gif")
+elif file_name.endswith(".jpg") or file_name.endswith(".jpeg") :
+    print("image/jpeg")
+elif file_name.endswith(".png"):
+    print("image/png")
+elif file_name.endswith(".pdf"):
+    print("application/pdf")
+elif file_name.endswith(".txt"):
+    print("text/plain")
+elif file_name.endswith(".zip"):
+    print("application/zip")
+
+
