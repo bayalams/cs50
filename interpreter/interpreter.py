6eb339aa1e99f26b6eb84020b5c4cc2dcb29e37e
bayalams <110493296+bayalams@users.noreply.github.com>
2023-07-26 18:45:43 +0000

    Wed, Jul 26, 2023, 7:45 PM +01:00

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -7,9 +7,27 @@ if len(expression.split(" ")) != 3:
 list_expression = expression.split(" ")
 print(list_expression)
 
+x = float(list_expression[0])
+y = list_expression[1]
+z = float(list_expression[2])
 
+print(x)
+print(y)
+print(z)
 
+if isnumeric(x) != True:
+    print('x must be a numeric character. For example: 1, 3.1, 1999')
+    print(f'debug: {x}')
+    exit(-1)
 
+if y not in ['+', '-', '*', '/']:
+    print('y must be an operator. For example: +, -, *, /')
+    print(f'debug: {y}')
+    exit(-1)
 
+if isnumeric(z) != True:
+    print('z must be a numeric character. For example: 1, 3.1, 1999')
+    print(f'debug: {z}')
+    exit(-1)
 
 
