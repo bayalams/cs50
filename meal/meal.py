67e4bbe33f9b1d8a90fe79017c9279b761045e60
bayalams <110493296+bayalams@users.noreply.github.com>
2023-07-26 21:40:01 +0000

    Wed, Jul 26, 2023, 10:40 PM +01:00

--- meal/meal.py
+++ meal/meal.py
@@ -4,26 +4,25 @@ def convert_hours(time):
 
     time = time.split(':')
     print(time)
-
-    if time[0].isnumeric() != True and time[1].isnumeric() != True:
-        print('error: values must be numerical. Words and letters are not supported.')
+    if len(time) != 2:
+        print('Error: must have two numbers separated by a colon.')
+        print(f'debug: {time}')
         exit(-1)
 
-    # hours = int(time[0])
-    # minutes = float(time[1]) / 60
+    hours = int(time[0])
+    minutes = float(time[1]) / 60
 
-    # if not 0 <= hours <= 24:
-    #     print('Value not supported. Hours must be between 0 and 24.')
-    #     print(f'debug: {hours}')
+    if not 0 <= hours <= 24:
+        print('Value not supported. Hours must be between 0 and 24.')
+        print(f'debug: {hours}')
 
-    # if not 0 <= minutes <= 1:
-    #     print('Value not supported. Minutes must be between 0 and 1')
-    #     print(f'debug: {minutes}')
-    #     exit(-1)
+    if not 0 <= minutes <= 1:
+        print('Value not supported. Minutes must be between 0 and 1')
+        print(f'debug: {minutes}')
+        exit(-1)
 
-    # s = hours + minutes
-    # print(s)
-    # return s
-
+    s = hours + minutes
+    print(s)
+    return s
 
 convert_hours(time)
