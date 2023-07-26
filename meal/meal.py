f6a93236ddfb9e3ced82792b371fbfb43f372caa
bayalams <110493296+bayalams@users.noreply.github.com>
2023-07-26 21:46:54 +0000

    Wed, Jul 26, 2023, 10:46 PM +01:00

--- meal/meal.py
+++ meal/meal.py
@@ -3,7 +3,7 @@ time = input('!')
 def convert_hours(time):
 
     time = time.split(':')
-    print(time)
+    print(f' time = time)
     if len(time) != 2:
         print('Error: must have two numbers separated by a colon.')
         print(f'debug: {time}')
@@ -14,11 +14,11 @@ def convert_hours(time):
 
     if not 0 <= hours <= 24:
         print('Value not supported. Hours must be between 0 and 24.')
-        print(f'debug: {hours}')
+        print(f'debug: hours = {hours}')
 
     if not 0 <= minutes <= 1:
         print('Value not supported. Minutes must be between 0 and 1')
-        print(f'debug: {minutes}')
+        print(f'debug: minutes = {minutes}')
         exit(-1)
 
     s = hours + minutes
@@ -26,3 +26,6 @@ def convert_hours(time):
     return s
 
 convert_hours(time)
+
+"""
+problemas: se o input for palavras ou letras, não está impedido;
