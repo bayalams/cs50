a08c05c58b87e87701080c80c4ba5bcca3b49d45
bayalams <110493296+bayalams@users.noreply.github.com>
2023-07-26 21:06:25 +0000

    Wed, Jul 26, 2023, 10:06 PM +01:00

--- meal/meal.py
+++ meal/meal.py
@@ -4,9 +4,13 @@ def convert_hours(time):
     time = time.split(':')
     print(time)
 
-    hours = float(time[0])
+    hours = int(time[0])
     minutes = float(time[1]) / 60
 
-    return float(f'{hours},{minutes}')
+    s = hours + minutes
+    #(f'{hours},{minutes}')
+    print(s)
+    return s
+
 
 convert_hours(time)
