a2f0aebc9fbe1c285f8c5616841371bd084d154c
bayalams <110493296+bayalams@users.noreply.github.com>
2023-07-26 21:22:15 +0000

    Wed, Jul 26, 2023, 10:22 PM +01:00

--- meal/meal.py
+++ meal/meal.py
@@ -7,12 +7,13 @@ def convert_hours(time):
     hours = int(time[0])
     minutes = float(time[1]) / 60
 
-    if not 0 < hours < 24:
+    if not 0 <= hours <= 24:
         print('Value not supported. Hours must be between 0 and 24.')
         exit(-1)
 
-    if not 0 < minutes < 60:
-        print('Value not supported. Minutes must be between 0 and 60')
+    if not 0 <= minutes <= 1:
+        print('Value not supported. Minutes must be between 0 and 1')
+        print('debug: )
         exit(-1)
 
 
