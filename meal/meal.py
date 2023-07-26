99b842d1585cf13cf2347a7c3d9c14bf41238d4f
bayalams <110493296+bayalams@users.noreply.github.com>
2023-07-26 21:01:21 +0000

    Wed, Jul 26, 2023, 10:01 PM +01:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,30 +1,4 @@
-s = input('time: ')
-s = s.split(':')
-print(s)
-
-a = float(s[0])
-b = float(s[1])
-
-if 0 < b < 15:
-    b = 0
-elif 11 < b < 20:
-    b = 0.3
-elif 21 < b < 30:
-    b = 0.
-elif 31 < b < 40:
-    b = 0.6
-elif 41 < b < 50:
-    b = 0.
-elif 51 < b <
-print(b)
-
-
-1 = 60
-0.5 = 30
-x = 20
-x = 40
-
-
+time = input('!')
 def convert_hours(time):
     s = s.split(':')
     print(s)
@@ -33,3 +7,5 @@ def convert_hours(time):
     minutes = float(s[1]) / 60
 
     return float(f'{hours},{minutes}')
+
+convert
