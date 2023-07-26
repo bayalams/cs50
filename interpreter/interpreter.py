51c031c1b6471b1e90620d8bc9f6d1a14a8d8abf
bayalams <110493296+bayalams@users.noreply.github.com>
2023-07-26 18:23:09 +0000

    Wed, Jul 26, 2023, 7:23 PM +01:00

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -1,6 +1,7 @@
 expression = input("expression: ")
 if len(expression.split(" ")) != 3:
     print("error: the input expression should contain two operands and one operator. usage example: 1 + 1.")
+    print(f"debug: {expression}")
     exit(-1)
 
 x, y, z = expression.split(" ")
