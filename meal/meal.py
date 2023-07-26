7562e3630b45f5119b418774f98db18cdffd1fcd
bayalams <110493296+bayalams@users.noreply.github.com>
2023-07-26 20:44:53 +0000

    Wed, Jul 26, 2023, 9:44 PM +01:00

--- meal/meal.py
+++ meal/meal.py
@@ -5,10 +5,16 @@ print(s)
 a = int(s[0])
 b = int(s[1])
 
-if b == 30:
-    b = 0.5
-else:
-    b = a + 0.5
-
+if 0 < b < 15:
+    b = 0
+elif 11 < b < 20:
+    b = 0.
+elif 21 < b < 30:
+    b = 0.
+elif 31 < b < 40:
+    b = 0.3
+elif 41 < b < 50:
+    b = 0.4
+elif 51 < b <
 print(b)
 
