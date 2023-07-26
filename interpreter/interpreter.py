984239f9b1962c0a0d7aa6ceed859befbd0b877e
bayalams <110493296+bayalams@users.noreply.github.com>
2023-07-26 18:16:10 +0000

    Wed, Jul 26, 2023, 7:16 PM +01:00

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -1,18 +1,4 @@
 expression = input("expression: ")
+
+
 x, y, z = expression.split(" ")
-listcenas = expression.splot(" " )
-
-if len(listcenas) != 3:
-    exit(-1)
-    
-x = listcenas[0]
-y = listcenas[1]
-z = listcenas[2]
-
-if (listcenas[1] == '+'):
-    listcenas[0] + listcenas[2]
-
-
-x = 1
-z = 1
-x, z = [1, 2]
