3dfaa2976e0e596ef87ba1f7643639548db7d64b
bayalams <110493296+bayalams@users.noreply.github.com>
2023-07-27 20:40:36 +0000

    Thu, Jul 27, 2023, 9:40 PM +01:00

--- meal/meal.py
+++ meal/meal.py
@@ -12,31 +12,31 @@ def main():
         exit(-1)
 
 
-# def convert_hours(time):
+def convert_hours(time):
 
-#     time = time.split(':') #único símbolo que separa os números e cria a lista. Inutiliza a utilização de outros símbolos.
-#     print(f'time = {time}')
+    time = time.split(':') #único símbolo que separa os números e cria a lista. Inutiliza a utilização de outros símbolos.
+    print(f'time = {time}')
 
-#     if len(time) != 2:
-#         print('Error: must have two numbers separated by a colon. Example: 12:34.')
-#         print(f'debug: {time}')
-#         exit(-1)
+    if len(time) != 2:
+        print('Error: must have two numbers separated by a colon. Example: 12:34.')
+        print(f'debug: {time}')
+        exit(-1)
 
-#     hours = int(time[0])
-#     minutes = (float(time[1]) / 60)
+    hours = int(time[0])
+    minutes = (float(time[1]) / 60)
 
-#     if not 0 <= hours <= 24:
-#         print('Value not supported. Hours must be between 0 and 24.')
-#         print(f'debug: hours = {hours}')
+    if not 0 <= hours <= 24:
+        print('Value not supported. Hours must be between 0 and 24.')
+        print(f'debug: hours = {hours}')
 
-#     if not 0 <= minutes <= 1:
-#         print('Value not supported. Minutes must be between 0 and 1')
-#         print(f'debug: minutes = {minutes}')
-#         exit(-1)
+    if not 0 <= minutes <= 1:
+        print('Value not supported. Minutes must be between 0 and 1')
+        print(f'debug: minutes = {minutes}')
+        exit(-1)
 
-#     s = hours + minutes
-#     print(s)
-#     return s
+    s = hours + minutes
+    print(s)
+    return s
 
 main()
 
