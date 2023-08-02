2415f032d88cc8e7935ccbdaa5d7a77ad3abd3b3
bayalams <110493296+bayalams@users.noreply.github.com>
2023-08-02 21:28:30 +0000

    Wed, Aug 2, 2023, 10:28 PM +01:00

--- camel/camel.py
+++ camel/camel.py
@@ -1,5 +1,12 @@
 case  = "camelCase"
+uppercase_letter = ""
 
 for i in case:
-    if i == i.isupper():
-        
+    if i.isupper():
+        uppercase_letter = i
+
+print(uppercase_letter)
+
+print(list(case))
+upper_list = case.split(uppercase_letter)
+print(upper_list)
