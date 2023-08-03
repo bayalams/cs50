0a1a29de969dfa7ff9c21b4d67a1ebf75c2a01a5
bayalams <110493296+bayalams@users.noreply.github.com>
2023-08-03 18:22:48 +0000

    Thu, Aug 3, 2023, 7:22 PM +01:00

--- camel/camel.py
+++ camel/camel.py
@@ -1,12 +1,12 @@
 case = "pythonIsTheBest"
-uppercase_letter = ""
+uppercase_letter = []
 
 for i in case:
     if i.isupper():
-        uppercase_letter = i
+        uppercase_letter.append(i)
 
 for i in case:
-    if i == uppercase_letter:
+    if i in uppercase_letter:
         case = case.replace(uppercase_letter, "_" + uppercase_letter)
 
 case = case.lower()
