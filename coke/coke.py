5459372f7270b1361651a6a4e47a8e78a21f3c2d
bayalams <110493296+bayalams@users.noreply.github.com>
2023-08-03 18:44:45 +0000

    Thu, Aug 3, 2023, 7:44 PM +01:00

--- coke/coke.py
+++ coke/coke.py
@@ -1,7 +1,11 @@
 #1. somar até 50 cents - def ammount_due
 #2. a partir de 50 cents, subtrair def change_owed
 
-ammount = int(input())
+ammount = int(input("insert coin: "))
+
+if ammount != 10 or ammount != 25 or ammount != 5:
+    print("Error: Machine only accepts coins of 5, 10 or 25 cents.")
+    exit(-1)
 
 
 
