32fb267330e221b5fada8dcf82d67bceff5cf275
bayalams <110493296+bayalams@users.noreply.github.com>
2023-08-03 18:26:33 +0000

    Thu, Aug 3, 2023, 7:26 PM +01:00

--- camel/camel.py
+++ camel/camel.py
@@ -1,13 +1,17 @@
 case = "pythonIsTheBest"
 uppercase_letter = []
 
+if case not isalpha():
+    print("Error: case must be comprised of letters. For example: camelCase")
+    exit(-1)
+
 for i in case:
     if i.isupper():
         uppercase_letter.append(i)
 
 for i in case:
     if i in uppercase_letter:
-        case = case.replace(uppercase_letter, "_" + uppercase_letter)
+        case = case.replace(i, "_" + i)
 
 case = case.lower()
 print(case)
