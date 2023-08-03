3f7526a3c898a9efe6d1197b040f5e3fa2671073
bayalams <110493296+bayalams@users.noreply.github.com>
2023-08-03 19:45:57 +0000

    Thu, Aug 3, 2023, 8:45 PM +01:00

--- coke/coke.py
+++ coke/coke.py
@@ -2,19 +2,19 @@
 #2. a partir de 50 cents, subtrair def change_owed
 
 amount = int(input("Insert coin: "))
+coke_price = 50
 
 if amount != 10 and amount != 25 and amount != 5:
     print("Error: Machine only accepts coins of 5, 10 or 25 cents.")
     exit(-1)
 
-coke_price = 50
 amount_due = coke_price - amount
 
-while amount_due > 0:
-    print(f"amount due: {amount_due}")
-    amount_due = amount_due
-    amount = int(input("Insert coin: "))
+while True:
+    amount_due = coke_price - amount
     break
+
+amount_due = amount_due - amount
 
 
 
