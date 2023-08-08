d0be215c21a610794c24a1e9046b911c3d93a968
bayalams <110493296+bayalams@users.noreply.github.com>
2023-08-08 15:44:39 +0000

    Tue, Aug 8, 2023, 4:44 PM +01:00

--- plates/plates.py
+++ plates/plates.py
@@ -10,13 +10,15 @@ list_plate = []
 if len(plate) < 2 or len(plate) > 6:
     print("1. Invalid.")
 else:
+    for i in plate[:2]:
+        if i.isalpha() != True:
+            print("2. Invalid.")
     for i in plate:
         if i.isalpha() or i.isnumeric():
             print(i)
             list_plate.append(i)
-    for i in plate[:2]:
-        if i.isalpha() != True:
-            print("2. Invalid.")
+            print(list_plate)
+
 
 
 
