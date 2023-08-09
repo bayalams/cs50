57ce6ec8071b9bafe8d60431ac1b8949c3684468
bayalams <110493296+bayalams@users.noreply.github.com>
2023-08-09 22:30:53 +0000

    Wed, Aug 9, 2023, 11:30 PM +01:00

--- plates/plates.py
+++ plates/plates.py
@@ -10,7 +10,9 @@ print(plate)
 
 if len(plate) < 2 or len(plate) > 6:
     print("1. Invalid.")
-    
+else:
+    pass
+
 for i in plate:
     if not (i.isalpha() or i.isnumeric()):
         print("2. Invalid")
