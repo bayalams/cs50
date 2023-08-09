ed8a5f0abcc24f55677c7dc10c4eaa68efa792c9
bayalams <110493296+bayalams@users.noreply.github.com>
2023-08-09 22:19:18 +0000

    Wed, Aug 9, 2023, 11:19 PM +01:00

--- plates/plates.py
+++ plates/plates.py
@@ -7,15 +7,20 @@ plate = input("Plate: ")
 plate = plate.upper()
 print(plate)
 
-while for i in plate:
+
+if len(plate) < 2 or len(plate) > 6:
+    print("1. Invalid.")
+
+for i in plate:
     if not (i.isalpha() or i.isnumeric()):
         print("2. Invalid")
-    elif len(plate) < 2 or len(plate) > 6:
-        print("1. Invalid.")
     else:
         for i in plate[:2]:
             if i.isalpha() != True:
                 ("3. Invalid.")
+else:
+    print("valid")
+
 
 
 
