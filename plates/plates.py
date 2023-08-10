e7c1c33f541b49902e5c5daf54017c5ed82e8763
bayalams <110493296+bayalams@users.noreply.github.com>
2023-08-10 20:32:47 +0000

    Thu, Aug 10, 2023, 9:32 PM +01:00

--- plates/plates.py
+++ plates/plates.py
@@ -46,11 +46,10 @@ def initial_char_type(plate):
 def nested_number(plate):
 
     a = 0
-    
+
     for i in plate:
         a = a + 1
         if i.isnumeric():
-            a = i
             for j in plate[:a]:
                 if j.isalpha():
                     return False
