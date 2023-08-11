8ef715972be0815b17a069cb1e122d80a0fdf3ce
bayalams <110493296+bayalams@users.noreply.github.com>
2023-08-11 19:24:56 +0000

    Fri, Aug 11, 2023, 8:24 PM +01:00

--- nutrition/python.py
+++ nutrition/python.py
@@ -2,6 +2,11 @@ fruit_calories: {
     "apple": 130,
     "avocado": 50,
     "banana": 110,
-    "cantaloupe": 50:
-    "grapefruit": 
+    "cantaloupe": 50,
+    "grapefruit": 60,
+    "grapes": 90,
+    "honeydew melon": 50,
+    "kiwifruit": 90,
+    "lemon": 15,
+    
 }
