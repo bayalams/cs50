15ee1021efef4c8bd5595ab25d1e7bad09c59167
bayalams <110493296+bayalams@users.noreply.github.com>
2023-08-11 19:20:18 +0000

    Fri, Aug 11, 2023, 8:20 PM +01:00

--- hello.py
+++
@@ -1,7 +0,0 @@
-name = input("what's your name? ")
-#print("hello, ", end = "")
-#the "end" bit overrides the default \n at the end of every print, which means that the name will appear right after "hello"
-#print(name)
-
-print(f'hello, {name}')
-
