08cbfdb09d6ed7ea69730255257c33afe945f42e
bayalams <110493296+bayalams@users.noreply.github.com>
2023-08-14 21:15:58 +0000

    Mon, Aug 14, 2023, 10:15 PM +01:00

--- nutrition/nutrition.py
+++ nutrition/nutrition.py
@@ -8,5 +8,15 @@ fruit_calories: {
     "honeydew melon": 50,
     "kiwifruit": 90,
     "lemon": 15,
-    
+    "lime": 20,
+    "nectarine": 60,
+    "orange": 80,
+    "peach": 60,
+    "pear": 100,
+    "pineapple": 50,
+    "plumes": 70,
+    "strawberries": 50,
+    "sweet cherries": 100,
+    "tangerine": 50,
+    "watermelon": 80
 }
