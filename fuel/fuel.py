3561c6ff7edf8d09257468fb2cd84e1411433d25
bayalams <110493296+bayalams@users.noreply.github.com>
2023-08-16 21:39:14 +0000

    Wed, Aug 16, 2023, 10:39 PM +01:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -4,14 +4,13 @@ while True:
     try:
         fuel = input("Fraction: ")
         fuel = fuel.split("/")
-        #print(fuel)
 
         X = int(fuel[0])
         Y = int(fuel[1])
 
         percentage = int((X / Y) * 100)
         print(f"{percentage}%")
-        
+
     except ZeroDivisionError:
         print("ZeroDivisionError")
     except ValueError:
