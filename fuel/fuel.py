5a93deab564c8a95b475f2da5a740cd59674bdb6
bayalams <110493296+bayalams@users.noreply.github.com>
2023-08-16 21:20:05 +0000

    Wed, Aug 16, 2023, 10:20 PM +01:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -14,7 +14,8 @@ while True:
         print(f"{percentage}%")
     # except ZeroDivisionError:
     #     print("ZeroDivisionError")
-    # except ValueError:
-    #     print("ValueError")
+    except ValueError:
+        print("ValueError")
+        
     except Exception as ex:
         print(f"An exception of type {type(ex).__name__} occurred. Arguments:\n{ex.args}")
