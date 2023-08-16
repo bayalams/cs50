11bab1e981c1033bbfd668e2377c2d1c4aa544eb
bayalams <110493296+bayalams@users.noreply.github.com>
2023-08-16 21:11:21 +0000

    Wed, Aug 16, 2023, 10:11 PM +01:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,15 +1,17 @@
 #X or Y is not an integer
 # X is greater than Y, or Y is 0, instead prompt the user again.
 
-try:
-    fuel = input("Fraction: ")
-    fuel = fuel.split("/")
-    #print(fuel)
+#try:
+fuel = input("Fraction: ")
+fuel = fuel.split("/")
+#print(fuel)
 
-    X = int(fuel[0])
-    Y = int(fuel[1])
+X = int(fuel[0])
+Y = int(fuel[1])
 
-    percentage = int((X / Y) * 100)
-    print(f"{percentage}%")
-except Exception as e:
-    print(e)
+percentage = int((X / Y) * 100)
+print(f"{percentage}%")
+except ZeroDivisionError:
+    print("ZeroDivisionError")
+ except ValueError:
+    print("ValueError")
