832012437c16ab38d21fe85d16e9e4e454a05c5e
bayalams <110493296+bayalams@users.noreply.github.com>
2023-08-18 20:22:54 +0000

    Fri, Aug 18, 2023, 9:22 PM +01:00

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -1,7 +1,15 @@
-item = input()
+# grocery list in all uppercase
+# sorted alphabetically by item
+# prefixing each line with the number of times the user inputted that item.
 
 
-
-
-except EOFError:
+while True:
+    try:
+        item = input()
+        item = item.upper()
+    except EOFError:
         break
+
+    list_item = []
+    list_item.append(item)
+    print(list_item)
