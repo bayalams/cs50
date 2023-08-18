73c73874be44e7bf62e0d3de525be3906b77ae6b
bayalams <110493296+bayalams@users.noreply.github.com>
2023-08-18 21:43:58 +0000

    Fri, Aug 18, 2023, 10:43 PM +01:00

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -2,7 +2,6 @@
 # sorted alphabetically by item
 # prefixing each line with the number of times the user inputted that item.
 
-list_item = []
 dict_item_count = {}
 dict_grocery_list = {}
 
@@ -10,20 +9,15 @@ while True:
     try:
         item = input()
         item = item.upper()
-        list_item.append(item)
-        sorted_list_item = sorted(list_item)
+        if item in dict_item_count.keys():
+            dict_item_count[item] = dict_item_count[item] + 1
+        else:
+            dict_item_count[item] = 1
 
     except KeyError:
         pass
     except EOFError:
         break
-
-for item in sorted_list_item:
-    if item in dict_item_count.keys():
-        dict_item_count[item] = dict_item_count[item] + 1
-    else:
-        dict_item_count[item] = 1
-
 
 for item, count in dict_item_count.items():
     dict_grocery_list[count] = item
