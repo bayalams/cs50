2ec247781aab9d7ab053d13655748c98abf3af33
bayalams <110493296+bayalams@users.noreply.github.com>
2023-08-20 19:37:38 +0000

    Sun, Aug 20, 2023, 8:37 PM +01:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -1,30 +1,29 @@
-date = input("Date: ")
-date = date.title()
-
-
 def parse_date_format_1(s):
     pass
 
 def parse_date_format_2(s):
     pass
 
-months = {
-    "January": 1,
-    "February": 2,
-    "March": 3,
-    "April": 4,
-    "May": 5,
-    "June": 6,
-    "July": 7,
-    "August": 8,
-    "September": 9,
-    "October": 10,
-    "November": 11,
-    "December": 12
-}
+months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
 
-if date_list[0] in months:
-    date_month = date_list[0]
+date = input("Date: ")
+print(date)
 
-print(date_month)
+if "," in date:
+    print("type 1")
+else:
+    print("type 2")
 
