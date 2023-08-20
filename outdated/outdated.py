ae17dff62852dd21fe82724c0f231203dbafaae7
bayalams <110493296+bayalams@users.noreply.github.com>
2023-08-20 13:44:09 +0000

    Sun, Aug 20, 2023, 2:44 PM +01:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -4,20 +4,18 @@ date_list = date.replace(",", " ").split()
 print(date_list)
 
 
-months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December"
-]
+months = {
+    "January": 1,
+    "February": 2,
+    "March": 3,
+    "April": 4,
+    "May": 5,
+    "June": 6,
+    "July": 7,
+    "August": 8,
+    "September": 9,
+    "October": 10,
+    "November": 11,
+    "December": 12
+}
 
-if date_list[0] in months:
-    print(date_list[0])
