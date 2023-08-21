592807540a58d701e028adfa687c0a2ffc2a0dfb
bayalams <110493296+bayalams@users.noreply.github.com>
2023-08-21 21:58:49 +0000

    Mon, Aug 21, 2023, 10:58 PM +01:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -9,19 +9,17 @@ def main():
     while True:
         try:
             date = input("Date: ")
-            #print(date)
+
             if "," in date:
-                #print("type 1")
                 new_date = parse_date_format_1(date)
                 print(new_date)
             else:
-                print("type 2")
                 new_date_2 = str(parse_date_format_2(date))
                 print(new_date_2)
         except FormatError:
             pass
-        except Exception as e:
-            print(e, type(e))
+        # except Exception as e:
+        #     print(e, type(e))
 
 
 def month_from_string_to_int(month):
@@ -41,12 +39,7 @@ def month_from_string_to_int(month):
         "December"
     ]
 
-    print("43", month)
     month = month.title()
-    print("45:", month)
-
-    print("47:", months.index(month))
-    print(str(months.index(month) + 1).zfill(2))
     return str(months.index(month) + 1).zfill(2) #para imprimir com o zero antes
 
 def parse_date_format_1(s):
@@ -56,7 +49,7 @@ def parse_date_format_1(s):
     month = list_of_elements[0]
     year = int(list_of_elements[2])
     aux_month = month_from_string_to_int(month)
-    print(aux_month)
+
 
     date_format(day, aux_month)
 
@@ -64,22 +57,16 @@ def parse_date_format_1(s):
 
 def parse_date_format_2(s):
     list_of_elements = s.split("/")
-    #print(list_of_elements)
 
     day = int(list_of_elements[1])
-    #print(f"day: {day}")
     month = int(list_of_elements[0])
-    #print(f"month: {month}")
     year = int(list_of_elements[2])
-    #print(f"year: {year}")
 
     date_format(day, month)
 
     return f"{year}-{(month):02d}-{day:02d}"
 
 def date_format(day, month):
-    print(f"day ={day}")
-    print(f"month ={month}")
 
     if day > 31:
         raise FormatError
