69f5579f5ff51a1addcc7fede3b261ed7bd7669c
bayalams <110493296+bayalams@users.noreply.github.com>
2023-08-21 20:51:17 +0000

    Mon, Aug 21, 2023, 9:51 PM +01:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -59,7 +59,8 @@ def main():
                 new_date_2 = str(parse_date_format_2(date))
                 print(new_date_2)
         except Exception as e:
-            print(f"Exception Name: {type(e)._name_}")
-            print(f"Exception Desc: {e}")
+            if month > 12:
+                pass
 
+            
 main()
