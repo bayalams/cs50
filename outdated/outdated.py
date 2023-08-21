b76d4d9b348b63ceee37008cd760f546a33ca009
bayalams <110493296+bayalams@users.noreply.github.com>
2023-08-21 17:28:01 +0000

    Mon, Aug 21, 2023, 6:28 PM +01:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -18,7 +18,6 @@ def month_from_string_to_int(month):
     month = month.title()
     return str(months.index(month) + 1).zfill(2) #para imprimir duas casas decimais
 
-
 def parse_date_format_1(s):
     list_of_elements = s.replace(",", "").split(" ")
 
@@ -30,27 +29,29 @@ def parse_date_format_1(s):
 
 def parse_date_format_2(s):
     list_of_elements = s.split("/")
-    print(list_of_elements)
+    #print(list_of_elements)
 
     day = list_of_elements[1]
-    print(f"day: {day}")
+    #print(f"day: {day}")
     month = list_of_elements[0]
-    print(f"month: {month}")
+    #print(f"month: {month}")
     year = list_of_elements[2]
-    print(f"year: {year}")
+    #print(f"year: {year}")
 
     return f"{year}-{(month)}-{day}"
 
+def main():
 
-date = input("Date: ")
-print(date)
+    date = input("Date: ")
+    print(date)
 
-if "," in date:
-    print("type 1")
-    new_date = parse_date_format_1(date)
-    print(new_date)
-else:
-    print("type 2")
-    new_date_2 = parse_date_format_2(date)
-    print(new_date_2)
+    if "," in date:
+        #print("type 1")
+        new_date = parse_date_format_1(date)
+        print(new_date)
+    else:
+        #print("type 2")
+        new_date_2 = parse_date_format_2(date)
+        print(new_date_2)
 
+main()
