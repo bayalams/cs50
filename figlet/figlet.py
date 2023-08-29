4a1447ac26f74e3a8b7504e0ec3b22bf9cebab02
bayalams <110493296+bayalams@users.noreply.github.com>
2023-08-29 22:06:28 +0000

    Tue, Aug 29, 2023, 11:06 PM +01:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -8,6 +8,7 @@ if len(sys.argv) == 1:
     print (figlet.renderText('text to render'))
     sys.exit()
 elif len(sys.argv) == 3:
+    print(sys.argv[2])
     figlet.setFont(font=sys.argv[2])
     print (figlet.renderText('text to render'))
     sys.exit()
