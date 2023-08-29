53681c6f4c4251306eef25ba251b2432a84b0e31
bayalams <110493296+bayalams@users.noreply.github.com>
2023-08-29 22:03:57 +0000

    Tue, Aug 29, 2023, 11:03 PM +01:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -2,11 +2,12 @@ from pyfiglet import Figlet
 import sys
 
 figlet = Figlet()
+print(len())
 
-if len(sys.argv) == 0:
+if len(sys.argv[]) == 0:
     print (figlet.renderText('text to render'))
     sys.exit()
-elif len(sys.argv) == 2:
+elif len(sys.argv[]) == 2:
     figlet.setFont(font=sys.argv[2])
     print (figlet.renderText('text to render'))
     sys.exit()
