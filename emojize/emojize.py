50be163aac0d29f345ba1ea1ad3199002bdb0ab8
bayalams <110493296+bayalams@users.noreply.github.com>
2023-08-29 21:19:12 +0000

    Tue, Aug 29, 2023, 10:19 PM +01:00

--- emojize/emojize.py
+++ emojize/emojize.py
@@ -1,5 +1,5 @@
 import emoji
 import sys
 
-print("hello, my name is", sys.argv[1])
-print(emoji.emojize('Python is ' + sys.argv[1]))
+cenas = input("Input: ")
+print(emoji.emojize("Output: " + cenas))
