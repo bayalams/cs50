921e190aabbcd9f7ae076f2ecdc46c937b4bfd4b
bayalams <110493296+bayalams@users.noreply.github.com>
2023-08-29 22:10:52 +0000

    Tue, Aug 29, 2023, 11:10 PM +01:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -3,15 +3,19 @@ import sys
 
 figlet = Figlet()
 print(len(sys.argv))
+f = sys.argv
 
 if len(sys.argv) == 1:
     print (figlet.renderText('text to render'))
     sys.exit()
 elif len(sys.argv) == 3:
-    print(sys.argv[2])
-    figlet.setFont(font=sys.argv[2])
-    print (figlet.renderText('text to render'))
-    sys.exit()
+    if sys.argv[1] != "-f":
+        print("Invalid user")
+        sys.exit()
+    else:
+        figlet.setFont(font=sys.argv[2])
+        print (figlet.renderText('text to render'))
+        sys.exit()
 else:
     print("Invalid usage")
     sys.exit()
