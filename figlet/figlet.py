070181f547bdf9111a7eeca75f9f8ce8e3e1eef4
bayalams <110493296+bayalams@users.noreply.github.com>
2023-08-29 22:30:27 +0000

    Tue, Aug 29, 2023, 11:30 PM +01:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -4,22 +4,20 @@ import sys
 figlet = Figlet()
 fonts = figlet.getFonts()
 
-print(len(sys.argv))
-
 if len(sys.argv) == 1:
     print (figlet.renderText(input("Input: ")))
     sys.exit()
 elif len(sys.argv) == 3:
     if sys.argv[1] != "-f":
-        print("2.Invalid usage")
+        print("Invalid usage")
         sys.exit()
     elif sys.argv[2] not in fonts:
-        print("1.Invalid Usage")
+        print("Invalid Usage")
         sys.exit()
     else:
         figlet.setFont(font=sys.argv[2])
         print (figlet.renderText(input("Input: ")))
         sys.exit()
 else:
-    print("3.Invalid usage")
+    print("Invalid usage")
     sys.exit()
