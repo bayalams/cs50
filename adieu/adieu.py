39e0faad1682afbfb75f8a1fa3e6241bed41b472
bayalams <110493296+bayalams@users.noreply.github.com>
2023-09-13 21:13:22 +0000

    Wed, Sep 13, 2023, 10:13 PM +01:00

--- adieu/adieu.py
+++ adieu/adieu.py
@@ -1,13 +1,10 @@
-import inflect
-
-p = inflect.engine()
 name_list = []
 
 while True:
     try:
         name = input("Name: ")
         print(f"1.{name}")
-        name_list = p.join([f"2.{name}"])
+        name_list.join(name)
         print(name_list)
     except EOFError:
         break
