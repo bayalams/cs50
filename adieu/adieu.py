f62c446969bdecf6896b95b18667f9c5f4adc371
bayalams <110493296+bayalams@users.noreply.github.com>
2023-09-13 20:18:53 +0000

    Wed, Sep 13, 2023, 9:18 PM +01:00

--- adieu/adieu.py
+++ adieu/adieu.py
@@ -5,7 +5,9 @@ p = inflect.engine()
 while True:
     try:
         name = input("Name: ")
-        name_list = p.join({name})
-        print(f"Adieu, adieu, to {name_list}")
+        #name_list = p.join({name})
+        #print(f"Adieu, adieu, to {name}")
     except EOFError:
         break
+    
+print(f"Adieu, adieu, to {name}")
