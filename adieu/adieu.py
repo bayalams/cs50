391917b89ade091d1175a689a6f63ab565e953f8
bayalams <110493296+bayalams@users.noreply.github.com>
2023-09-14 17:45:09 +0000

    Thu, Sep 14, 2023, 6:45 PM +01:00

--- adieu/adieu.py
+++ adieu/adieu.py
@@ -4,7 +4,7 @@ while True:
     try:
         name = input("Name: ")
         print(f"1.{name}")
-        name_list = name_list.append(name)
+        name_list.append(name)
         print(name_list)
     except EOFError:
         break
