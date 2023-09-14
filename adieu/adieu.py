356ae0f6cbe9ebe8e597ff9b27be3bb11308a2e8
bayalams <110493296+bayalams@users.noreply.github.com>
2023-09-14 17:48:28 +0000

    Thu, Sep 14, 2023, 6:48 PM +01:00

--- adieu/adieu.py
+++ adieu/adieu.py
@@ -1,4 +1,8 @@
+import inflect
+p = inflect.engine()
+
 name_list = []
+adieu_list = []
 
 while True:
     try:
@@ -6,6 +10,8 @@ while True:
         print(f"1.{name}")
         name_list.append(name)
         print(name_list)
+        adieu_list = p.join(name_list)
+        print(f"Adieu, adieu, to {adieu_list}")
     except EOFError:
         break
 
