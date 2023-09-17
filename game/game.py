dd6c2279898c05d8ad11898f5c0ad0b27699d967
bayalams <110493296+bayalams@users.noreply.github.com>
2023-09-17 16:54:24 +0000

    Sun, Sep 17, 2023, 5:54 PM +01:00

--- game/game.py
+++ game/game.py
@@ -1,7 +1,17 @@
 import random
 
-
-level = int(input("Level: "))
-random_number = random.randint(1, level)
-print(f"Random number: {random_number}")
-guess = int(input("Guess: "))
+while True:
+    try:
+        level = int(input("Level: "))
+        random_number = random.randint(1, level)
+    except ValueError:
+        break
+    else:
+        print(f"Random number: {random_number}")
+        guess = int(input("Guess: "))
+        if guess < random_number:
+            print("Too small!")
+        elif guess > random_number:
+            print("Too large!")
+        else:
+            print("Just right!")
