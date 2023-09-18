67dabe5f914f8c8c24f478fc46c7f8c8fe80042c
bayalams <110493296+bayalams@users.noreply.github.com>
2023-09-18 17:16:42 +0000

    Mon, Sep 18, 2023, 6:16 PM +01:00

--- game/game.py
+++ game/game.py
@@ -15,20 +15,29 @@ def guess_range():
 
 def guess_number(random_number):
 
-    while True:
-        guess = int(input("Guess: "))
+    guess = int(input("Guess: "))
+    if guess > 0 or guess != int:
+        pass
+
+    while guess != random_number:
         try:
             if guess < random_number:
                 if guess < 1:
                     guess = int(input("Guess-1: "))
+                    if guess > 0 or guess != int:
+                        pass
                 else:
                     print("paragem 1")
                     print("Too small!")
                     guess = int(input("Guess1: "))
+                    if guess > 0 or guess != int:
+                        pass
             elif guess > random_number:
                 print("paragem 2")
                 print("Too large!")
                 guess = int(input("Guess2: "))
+                if guess > 0 or guess != int:
+                    pass
             else:
                 print("Just right!")
                 break
