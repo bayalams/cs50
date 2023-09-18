83be463c756c77829b29ccfc6e194801e786c21d
bayalams <110493296+bayalams@users.noreply.github.com>
2023-09-18 17:27:45 +0000

    Mon, Sep 18, 2023, 6:27 PM +01:00

--- game/game.py
+++ game/game.py
@@ -23,38 +23,38 @@ def define_guess_number(guess):
             else:
                 pass
 
-def main():
+# def main():
 
-    while guess != random_number:
-        guess = input("Guess-2: ")
-        if guess != int or guess > 0:
-            pass
-        try:
-            if guess < random_number:
-                if guess < 1:
-                    guess = int(input("Guess-1: "))
-                    if guess > 0 or guess != int:
-                        pass
-                else:
-                    print("paragem 1")
-                    print("Too small!")
-                    guess = int(input("Guess1: "))
-                    if guess > 0 or guess != int:
-                        pass
-            elif guess > random_number:
-                print("paragem 2")
-                print("Too large!")
-                guess = int(input("Guess2: "))
-                if guess > 0 or guess != int:
-                    pass
-            else:
-                print("Just right!")
-                break
-        except ValueError:
-            pass
+#     # while guess != random_number:
+#         guess = input("Guess-2: ")
+#         if guess != int or guess > 0:
+#             pass
+#         try:
+#             if guess < random_number:
+#                 if guess < 1:
+#                     guess = int(input("Guess-1: "))
+#                     if guess > 0 or guess != int:
+#                         pass
+#                 else:
+#                     print("paragem 1")
+#                     print("Too small!")
+#                     guess = int(input("Guess1: "))
+#                     if guess > 0 or guess != int:
+#                         pass
+#             elif guess > random_number:
+#                 print("paragem 2")
+#                 print("Too large!")
+#                 guess = int(input("Guess2: "))
+#                 if guess > 0 or guess != int:
+#                     pass
+#             else:
+#                 print("Just right!")
+#                 break
+#         except ValueError:
+#             pass
 
 
 
-if __name__ == "__main__":
-    random_number = guess_range()
-    guess_number(random_number)
+# if __name__ == "__main__":
+#     random_number = guess_range()
+#     guess_number(random_number)
