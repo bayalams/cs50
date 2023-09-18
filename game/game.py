bf85a396e1a0e6f780b4888b62b6d4df4048c4d6
bayalams <110493296+bayalams@users.noreply.github.com>
2023-09-18 17:56:44 +0000

    Mon, Sep 18, 2023, 6:56 PM +01:00

--- game/game.py
+++ game/game.py
@@ -1,14 +1,14 @@
 import random
 
-
-def main(guess, random_number):
-
-    if guess > random_number:
-       print("Too large")
-    elif guess < random_number:
-       print("Too small")
-    else:
-       print("Just right")
+def main(guess, random_number): #this also needs to be a while loop!!!!!! otherwise it gives the answer and it exits the program; como está, dá loop infinito
+    while True:
+        if guess > random_number:
+            print("Too large")
+        elif guess < random_number:
+            print("Too small")
+        else:
+            print("Just right")
+            break
 
 
 def guess_range():
@@ -24,7 +24,7 @@ def guess_range():
             pass
 
 
-def define_guess_number(guess):
+def define_guess_number(): #não tem argumento porque o argumento é wildcard, o mesmo com o de cima
     while True:
         try:
             guess = int(input("Guess: "))
@@ -40,6 +40,6 @@ def define_guess_number(guess):
 
 
 if __name__ == "__main__":
+    random_number = guess_range() #a ordem é importante!!!!
     guess = define_guess_number() #got it, estamos a atribuir à variável o resultado da função para podermos usar noutra função
-    random_number = guess_range()
     main(guess, random_number)
