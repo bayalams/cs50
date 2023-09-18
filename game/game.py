a7d6503f2b407e9e6c97c5bc4b7c45a5a2862c14
bayalams <110493296+bayalams@users.noreply.github.com>
2023-09-18 17:45:43 +0000

    Mon, Sep 18, 2023, 6:45 PM +01:00

--- game/game.py
+++ game/game.py
@@ -1,5 +1,37 @@
 import random
 
+
+def main():
+
+    while guess != random_number:
+        guess = input("Guess-2: ")
+        if guess != int or guess > 0:
+            pass
+        try:
+            if guess < random_number:
+                if guess < 1:
+                    guess = int(input("Guess-1: "))
+                    if guess > 0 or guess != int:
+                        pass
+                else:
+                    print("paragem 1")
+                    print("Too small!")
+                    guess = int(input("Guess1: "))
+                    if guess > 0 or guess != int:
+                        pass
+            elif guess > random_number:
+                print("paragem 2")
+                print("Too large!")
+                guess = int(input("Guess2: "))
+                if guess > 0 or guess != int:
+                    pass
+            else:
+                print("Just right!")
+                break
+        except ValueError:
+            pass
+
+
 def guess_range():
 
     while True:
@@ -24,36 +56,7 @@ def define_guess_number(guess):
             pass #why not continue????
         else:
             print(f"define guess: {guess}")
-
-# def main():
-
-#     # while guess != random_number:
-#         guess = input("Guess-2: ")
-#         if guess != int or guess > 0:
-#             pass
-#         try:
-#             if guess < random_number:
-#                 if guess < 1:
-#                     guess = int(input("Guess-1: "))
-#                     if guess > 0 or guess != int:
-#                         pass
-#                 else:
-#                     print("paragem 1")
-#                     print("Too small!")
-#                     guess = int(input("Guess1: "))
-#                     if guess > 0 or guess != int:
-#                         pass
-#             elif guess > random_number:
-#                 print("paragem 2")
-#                 print("Too large!")
-#                 guess = int(input("Guess2: "))
-#                 if guess > 0 or guess != int:
-#                     pass
-#             else:
-#                 print("Just right!")
-#                 break
-#         except ValueError:
-#             pass
+            return guess
 
 
 
