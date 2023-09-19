db323e14de5260d919bbda2154de042237ea9c6d
bayalams <110493296+bayalams@users.noreply.github.com>
2023-09-19 20:36:07 +0000

    Tue, Sep 19, 2023, 9:36 PM +01:00

--- professor/professor.py
+++ professor/professor.py
@@ -1,17 +1,16 @@
 def get_level():
 
     while True:
-        level  = int(input("Level1: "))
+        try:
+            level  = int(input("Level1: "))
 
-        if level not in (1, 2, 3):
-            level = int(input("Level2: "))
-        else:
-            print(f"The level is {level}")
-        raise ValueError
-
-    return level
-
-
+            if level not in (1, 2, 3):
+                level = int(input("Level2: "))
+            else:
+                print(f"The level is {level}")
+                return level
+        except ValueError:
+            continue
 
 get_level()
 
