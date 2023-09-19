30e11812806677f658ba10e48050ecb174254443
bayalams <110493296+bayalams@users.noreply.github.com>
2023-09-19 20:25:35 +0000

    Tue, Sep 19, 2023, 9:25 PM +01:00

--- professor/professor.py
+++ professor/professor.py
@@ -1,9 +1,12 @@
-import random
+def get_level():
 
-level  = input("Level: ")
+    level  = int(input("Level1: "))
 
-while True:
-    if level not in (1, 2, 3):
-        level = input("Level: ")
-    else:
-        break
+    while True:
+        if level not in (1, 2, 3):
+            level = int(input("Level2: "))
+            return level
+        else:
+            break
+
+get_level()
