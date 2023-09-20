961f5ca7fa7453531db9821bc2a4916f4300d5c2
bayalams <110493296+bayalams@users.noreply.github.com>
2023-09-20 17:56:26 +0000

    Wed, Sep 20, 2023, 6:56 PM +01:00

--- professor/professor.py
+++ professor/professor.py
@@ -11,6 +11,9 @@ def main(random_int_1, random_int_2):
         random_int_1, random_int_2 = generate_digits(level) #needs to be inside the loop so that it keeps generating new random ints
         result = random_int_1 + random_int_2
         result_list.append(result)
+        print(f"{random_int_1} + {random_int_2} = ")
+        input("result: ")
+
 
 
 def get_level(): #number of digits of random ints
