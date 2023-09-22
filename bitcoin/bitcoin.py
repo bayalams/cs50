6a566e3b7917e3d4ded993cbe3c4c81a3f820e21
bayalams <110493296+bayalams@users.noreply.github.com>
2023-09-22 18:30:33 +0000

    Fri, Sep 22, 2023, 7:30 PM +01:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 def print_json(d):
-    return json.dumps(d)
+    return json.dumps(d, indent=2)
 
 # while True:
 #     try:
@@ -14,11 +14,15 @@ def print_json(d):
 #     except requests.RequestException:
 #         continue
 
+# HTTP GET request to URL "api.coindesk.com" for asset/path "v1/bpi/currentprice.json"
 r = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
+# Response in "dict" format.
 response = r.json()
 print(print_json(response), type(response))
+
 print()
 bpi = response["bpi"]["USD"]
+print(print_json(bpi))
 print(bpi["rate_float"])
 
 
