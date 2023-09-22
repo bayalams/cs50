eb75b13ab363a8f90fd89d3de9c0368f12abd188
bayalams <110493296+bayalams@users.noreply.github.com>
2023-09-22 18:17:12 +0000

    Fri, Sep 22, 2023, 7:17 PM +01:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -1,17 +1,17 @@
 import requests
 import sys
 
-while True:
-    try:
-        bitcoin_amount = float(input("Amount of Bitcoin: "))
-    except ValueError:
-        continue
-    except TypeError:
-        continue
-    except requests.RequestException:
-        continue
+# while True:
+#     try:
+#         bitcoin_amount = float(input("Amount of Bitcoin: "))
+#     except ValueError:
+#         continue
+#     except TypeError:
+#         continue
+#     except requests.RequestException:
+#         continue
 
-r = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json", "bpi" {"USD" {"rate"}})
+r = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
 print(r)
 
 
