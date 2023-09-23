d1783676837525bce535a0e204efcdadb5a03316
bayalams <110493296+bayalams@users.noreply.github.com>
2023-09-23 12:59:01 +0000

    Sat, Sep 23, 2023, 1:59 PM +01:00

--- experiment/experiment.py
+++ experiment/experiment.py
@@ -1,12 +1,4 @@
-result_list = [1, 2, 3, 4, 5]
-user_input = input("result: ")
-user_input_list = []
-user_input_list.append(user_input)
-score_list = []
+import sys
 
-
-while len(user_input_list) != 5:
-    for user_input in user_input_list:
-            for result in result_list:
-                if user_input == result:
-                    score_list.append(user_input)
+print(len(sys.argv[]))
+print("Hello, my name is", sys.argv[1])
