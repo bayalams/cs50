ffea22df4cec4a91e139bd943425ecf32ab89b68
bayalams <110493296+bayalams@users.noreply.github.com>
2023-09-23 12:57:14 +0000

    Sat, Sep 23, 2023, 1:57 PM +01:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -4,7 +4,13 @@ import sys
 
 def main(rate):
 
-    
+    bitcoin_amount = sys.argv[1]
+
+    if len(sys.argv) < 2:
+        sys.exit("Too few arguments")
+    elif len(sys.argv) > 2:
+        sys.exit("Too many arguments")
+
 
     # while True: #while what is True? argv[]!!!!!
     #     try:
