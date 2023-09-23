a08d2a970a314285e798b32a8808cb171fde0bdf
bayalams <110493296+bayalams@users.noreply.github.com>
2023-09-23 21:20:02 +0000

    Sat, Sep 23, 2023, 10:20 PM +01:00

--- professor/professor.py
+++ professor/professor.py
@@ -16,6 +16,7 @@ def main():
     user_answers = []
     current_question = 0
     already_failed = False
+    score = 0
 
     # 3. while user_answers are not the same as the number of questions, keep going...
     while len(user_answers) != NUMBER_OF_QUESTIONS:
@@ -28,34 +29,18 @@ def main():
 
             if user_result == desired_result:
                 if not already_failed:
-                    user_answers.append(user_result)
+                    score = score + 1
                 current_question = current_question + 1
                 already_failed = False
 
             elif user_result != desired_result:
-                if not already_failed:
-                    user_answers.append("EEE")
                 already_failed = True
                 print("EEE")
 
         except ValueError:
             continue
 
-    # print(f"result list: {result_list}")
-    # print(f"user results: {user_result_list}")
-
-    score_list = []
-    #------->>>>>>> ERRADO, VOLTAR AQUI!!! <<<<------
-    for result, user_result in zip(result_list, user_result_list):
-        if user_result == result:
-            score_list.append(user_result)
-    #score_list vai buscar números duplicados, whyyy????
-    #------->>>>>>> ERRADO, VOLTAR AQUI!!! <<<<------
-
-    # print()
-    # print(f"length of score list: {len(score_list)}")
-    # print(f"score list: {score_list}")
-    print(f"Score: {len(score_list)}")
+    print(f"Score: {score)")
 
 def get_level(): #number of digits of random ints
 
