46ea91b4305a222439a6ccfb14b44ae35358c613
bayalams <110493296+bayalams@users.noreply.github.com>
2023-09-23 11:19:55 +0000

    Sat, Sep 23, 2023, 12:19 PM +01:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -1,36 +1,43 @@
 import requests
 import json
 
+def main(rate):
+
+    while True:
+        try:
+            bitcoin_amount = float(input("Amount of Bitcoin: "))
+        except ValueError:
+            continue
+        except TypeError:
+            continue
+        except requests.RequestException:
+            continue
+        break #isto está errado
+
+    bitcoin_price = bitcoin_amount * rate
+    print(bitcoin_amount)
+
 def print_json(d):
     return json.dumps(d, indent=2)
 
-while True:
-    try:
-        bitcoin_amount = float(input("Amount of Bitcoin: "))
-    except ValueError:
-        continue
-    except TypeError:
-        continue
-    except requests.RequestException:
-        continue
-    break
+
+def return_rate():
+
+    # HTTP GET request to URL "api.coindesk.com" for asset/path "v1/bpi/currentprice.json"
+    r = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
+    # Response in "dict" format.
+    response = r.json()
+    # print(print_json(response), type(response))
+    # print()
+
+    # Get "bpi" -> "USD" -> "rate_float" from response
+    bpi = response["bpi"]["USD"]
+    # print(print_json(bpi))
+
+    rate = bpi["rate_float"]
+    print(f"${rate:,.4f}")
+    return f"${rate:,.4f}"
 
 
-
-# HTTP GET request to URL "api.coindesk.com" for asset/path "v1/bpi/currentprice.json"
-r = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
-# Response in "dict" format.
-response = r.json()
-# print(print_json(response), type(response))
-# print()
-
-# Get "bpi" -> "USD" -> "rate_float" from response
-bpi = response["bpi"]["USD"]
-# print(print_json(bpi))
-
-rate = bpi["rate_float"]
-print(f"${rate:,.4f}")
-
-
-
-
+rate = return_rate()
+main(rate)
