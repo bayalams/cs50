d3d7b5621775cf771c3cffe191486c8ef885b09e
bayalams <110493296+bayalams@users.noreply.github.com>
2023-09-25 22:26:00 +0000

    Mon, Sep 25, 2023, 11:26 PM +01:00

--- test_bank.py/test_bank.py
+++ test_bank.py/test_bank.py
@@ -1,7 +1,10 @@
 from bank import value
 
-def test_value(greeting):
+def test_value():
 
-    if not greeting.isalpha():
+    if "3333" == "$100":
         print("Wrong")
+
+if __name__ == "__main__":
+    test_value()
 
