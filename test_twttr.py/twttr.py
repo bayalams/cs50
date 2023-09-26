7193709b92ad053b7305e941293dd663d4cb03ba
bayalams <110493296+bayalams@users.noreply.github.com>
2023-09-26 12:43:23 +0000

    Tue, Sep 26, 2023, 1:43 PM +01:00

--- test_twttr.py/twttr.py
+++ test_twttr.py/twttr.py
@@ -11,9 +11,9 @@ def shorten(s):
     consonants = []
 
     for i in s:
-        # if i.isalpha() == False:
-        #     print("not a word")
-        #     exit(1)
+        if i.isalpha() == False:
+            print("not a word")
+            exit(1)
         if i not in vowels:
             consonants.append(i)
 
