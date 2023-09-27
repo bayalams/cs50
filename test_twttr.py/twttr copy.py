102190db32d66f306c867faabf9e87a39c47c50e
bayalams <110493296+bayalams@users.noreply.github.com>
2023-09-27 20:27:35 +0000

    Wed, Sep 27, 2023, 9:27 PM +01:00

---
+++ test_twttr.py/twttr copy.py
@@ -0,0 +1,22 @@
+def main():
+
+    s = input("Insert string: ")
+    print(shorten(s))
+    return shorten(s)
+
+
+def shorten(s):
+
+    vowels = "aeiouAEIOU"
+    consonants = []
+
+    for i in s:
+        if i not in vowels:
+            consonants.append(i)
+
+    str_consonants = ''.join(consonants)
+    return str_consonants
+
+if __name__ == "__main__":
+    main()
+
