3a0f5a370acba4bf5945d816370f18abbb593cde
bayalams <110493296+bayalams@users.noreply.github.com>
2023-09-27 20:02:50 +0000

    Wed, Sep 27, 2023, 9:02 PM +01:00

--- test_bank/bank.py
+++ test_bank/bank.py
@@ -1,18 +1,18 @@
 
 def main():
     greeting = input("Greeting: ")
-    print(value(greeting))
+    print(f"${value(greeting)}")
 
 
 def value(greeting):
     greeting = greeting.lower().strip()
 
     if greeting[0:5] == "hello":
-        return "$0"
+        return 0
     elif greeting[0] == "h":
-        return "$20"
+        return 20
     else:
-        return "$100"
+        return 100
 
 
 if __name__ == "__main__":
