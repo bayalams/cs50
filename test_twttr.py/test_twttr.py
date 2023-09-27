b38fa9273ed0d3b104721e96e4c1ffd56063a5a1
bayalams <110493296+bayalams@users.noreply.github.com>
2023-09-27 20:35:20 +0000

    Wed, Sep 27, 2023, 9:35 PM +01:00

--- test_twttr.py/test_twttr.py
+++ test_twttr.py/test_twttr.py
@@ -6,6 +6,7 @@ def main():
 
     test_shorten_lower()
     test_shorten_upper()
+    ommit_numbers()
 
 def test_shorten_lower():
     assert shorten("macaco") == "mcc"
@@ -16,6 +17,8 @@ def test_shorten_upper():
     assert shorten("MACACO") == "MCC"
     assert shorten("BEATRIZ") == "BTRZ"
 
+def ommit_numbers():
+    assert shorten("BEATRIZ2") == "BTRZ2"
 
 if __name__ == "__main__":
     main()
