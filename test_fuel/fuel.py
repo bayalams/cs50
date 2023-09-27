c9a1ccedb3c04d54a0c1351071b96f050307f5e3
bayalams <110493296+bayalams@users.noreply.github.com>
2023-09-27 21:32:54 +0000

    Wed, Sep 27, 2023, 10:32 PM +01:00

---
+++ test_fuel/fuel.py
@@ -0,0 +1,29 @@
+def fraction():
+
+    fuel = input("Fraction: ")
+    fuel = fuel.split("/")
+
+    X = int(fuel[0])
+    Y = int(fuel[1])
+
+    return round((X / Y) * 100)
+
+while True:
+    try:
+        percentage = fraction()
+
+        if percentage in [99, 100]:
+            print("F")
+            break
+        elif percentage <= 1:
+            print("E")
+            break
+        elif percentage > 1 and percentage < 99:
+            print(f"{percentage}%")
+            break
+
+    except ZeroDivisionError:
+        pass
+    except ValueError:
+        pass
+
