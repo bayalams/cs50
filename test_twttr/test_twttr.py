c6320207813afadf5fe9b5d421a6cbf3fc1855dc
bayalams <110493296+bayalams@users.noreply.github.com>
2023-09-27 20:41:10 +0000

    Wed, Sep 27, 2023, 9:41 PM +01:00

--- test_twttr/test_twttr.py
+++ test_twttr/test_twttr.py
@@ -6,7 +6,7 @@ def main():
 
     test_shorten_lower()
     test_shorten_upper()
-    ommit_numbers()
+    test_ommit_numbers()
 
 def test_shorten_lower():
     assert shorten("macaco") == "mcc"
@@ -17,8 +17,11 @@ def test_shorten_upper():
     assert shorten("MACACO") == "MCC"
     assert shorten("BEATRIZ") == "BTRZ"
 
-def ommit_numbers():
+def test_ommit_numbers():
     assert shorten("BEATRIZ2") == "BTRZ2"
+
+def test_ommit_punctuation():
+    assert shorten("What's up?") == "Wht's p?"
 
 if __name__ == "__main__":
     main()
