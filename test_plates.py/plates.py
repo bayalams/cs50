80cf80932f4ec2e3fe98bd94553ec7382942665b
bayalams <110493296+bayalams@users.noreply.github.com>
2023-09-27 18:16:40 +0000

    Wed, Sep 27, 2023, 7:16 PM +01:00

--- test_plates.py/plates.py
+++ test_plates.py/plates.py
@@ -1,48 +1,4 @@
 
-def main():
-    plate = input("Plate: ")
-    if is_valid(plate):
-        print("Valid")
-        return
-    else:
-        print("Invalid")
-        return
-
-
-def is_valid(plate):
-
-    # if length(plate) and char_type(plate) and initial_char_type(plate) and nested_number(plate):
-    #     return True
-
-    #all wrong
-    for i in plate:
-        if 2 < len(plate) < 6 or (i.isalpha() or i.isnumeric()):
-            for j in plate[:2]:
-                if j.isalpha() == True:
-                    return True
-                return True
-        else:
-            return False
-
-
-
-    a = -1
-
-    for i in plate:
-        a = a + 1
-        if i.isnumeric():
-            if i == "0":
-                return False
-            for j in plate[a:]:
-                if j.isalpha():
-                    return False
-                else:
-                    pass
-            return True
-        else:
-            pass
-
-    return True
 
 
 if __name__ == "__main__":
