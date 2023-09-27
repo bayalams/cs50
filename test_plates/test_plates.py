9205adf3ac802a1e5208f891053f75dd4257eddc
bayalams <110493296+bayalams@users.noreply.github.com>
2023-09-27 20:52:38 +0000

    Wed, Sep 27, 2023, 9:52 PM +01:00

--- test_plates/test_plates.py
+++ test_plates/test_plates.py
@@ -5,5 +5,8 @@ def test_length():
     assert is_valid("cs50") == True
     assert is_valid("c") == False
 
-
+def test_char_type():
+    assert is_valid("cs50") == True
+    assert is_valid("3erf") == False
+    assert is_valid("34rf") == False
 
