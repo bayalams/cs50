2658eaea48125871f8df061db8d9379486aa36f8
bayalams <110493296+bayalams@users.noreply.github.com>
2023-09-28 17:13:03 +0000

    Thu, Sep 28, 2023, 6:13 PM +01:00

--- test_fuel/fuel.py
+++ test_fuel/fuel.py
@@ -1,27 +1,38 @@
+def main():
+    fraction = input("Fraction: ")
+    convert(fraction)
+    gauge(percentage)
 
 
 def convert(fraction):
 
     while True:
         try:
-            fraction = in
             fraction = fraction.split("/")
+            print(fraction)
             X = int(fraction[0])
             Y = int(fraction[1])
             percentage = round((X / Y) * 100)
+            print(percentage)
             return percentage
+
         except ZeroDivisionError:
             pass
         except ValueError:
             pass
 
-
 def gauge(percentage):
+
     if percentage in [99, 100]:
+        print("F")
         return "F"
     elif percentage <= 1:
+        print("E")
         return "E"
     elif percentage > 1 and percentage < 99:
+        print(f"{percentage}%")
         return f"{percentage}%"
 
 
+if __name__ == "__main__":
+    main()
