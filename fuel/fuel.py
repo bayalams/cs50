f159a2b518765b5a4a2ad4e2e1d5bc4da30f0ee4
bayalams <110493296+bayalams@users.noreply.github.com>
2023-09-28 17:30:38 +0000

    Thu, Sep 28, 2023, 6:30 PM +01:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,14 +1,13 @@
-fuel = input("Fraction: ")
-fuel = fuel.split("/")
 
-X = int(fuel[0])
-Y = int(fuel[1])
-
-return round((X / Y) * 100)
-
-
+while True:
     try:
-        
+        fuel = input("Fraction: ")
+        fuel = fuel.split("/")
+
+        X = int(fuel[0])
+        Y = int(fuel[1])
+
+        percentage = round((X / Y) * 100)
 
         if percentage in [99, 100]:
             print("F")
