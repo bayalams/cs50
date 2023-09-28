af0e6b2d5a71d38d5e66253af99e5d8744673154
bayalams <110493296+bayalams@users.noreply.github.com>
2023-09-28 17:40:22 +0000

    Thu, Sep 28, 2023, 6:40 PM +01:00

--- test_fuel/fuel.py
+++ test_fuel/fuel.py
@@ -1,49 +1,36 @@
-def fraction():
+def main():
+    fraction()
+    gauge(percentage)
 
+def fraction():
     while True:
         try:
             fuel = input("Fraction: ")
             fuel = fuel.split("/")
+            X = int(fuel[0])
+            Y = int(fuel[1])
+            print(X, Y)
 
-    except ZeroDivisionError:
-        pass
-    except ValueError:
-        pass
-    except IndexError:
-        pass
+            percentage = round((X / Y) * 100)
+            return percentage
 
-        X = int(fuel[0])
-        Y = int(fuel[1])
-        print(X, Y)
-
-        percentage = round((X / Y) * 100)
-        return percentage
-
+        except ZeroDivisionError:
+            pass
+        except ValueError:
+            pass
+        except IndexError:
+            pass
 
 
-while True:
-    try:
-        fuel = input("Fraction: ")
-        fuel = fuel.split("/")
-
-        X = int(fuel[0])
-        Y = int(fuel[1])
-
-        percentage = round((X / Y) * 100)
-
+def gauge(percentage):
         if percentage in [99, 100]:
             print("F")
-            break
         elif percentage <= 1:
             print("E")
-            break
         elif percentage > 1 and percentage < 99:
             print(f"{percentage}%")
-            break
 
-    except ZeroDivisionError:
-        pass
-    except ValueError:
-        pass
-    except IndexError:
-        pass
+
+if __name__ == "__main__":
+    percentage = fraction()
+    main()
