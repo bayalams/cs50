119e0db0dc5684bee2f435aec05d95f045d57ae0
bayalams <110493296+bayalams@users.noreply.github.com>
2023-09-30 18:29:02 +0000

    Sat, Sep 30, 2023, 7:29 PM +01:00

--- test_fuel/test_fuel.py
+++ test_fuel/test_fuel.py
@@ -8,7 +8,7 @@ def main():
 def test_convert():
     assert convert("1/3") == 33
     with pytest.raises(ZeroDivisionError):
-        convert("0")
+        convert("1/0")
 
 if __name__ == "__main__":
     main()
