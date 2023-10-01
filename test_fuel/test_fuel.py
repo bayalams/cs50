26d7b473ad485f6f521cb8f1f2728af6bf2f824b
bayalams <110493296+bayalams@users.noreply.github.com>
2023-10-01 15:10:39 +0000

    Sun, Oct 1, 2023, 4:10 PM +01:00

--- test_fuel/test_fuel.py
+++ test_fuel/test_fuel.py
@@ -11,8 +11,8 @@ def test_convert():
         convert("1/0")
     with pytest.raises(IndexError):
         convert("1")
-    with pytest.raises(IndexError):
-        convert("1")
+    with pytest.raises(ValueError):
+        convert("cat")
 
 
 if __name__ == "__main__":
