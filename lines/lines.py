17222cf1a4746eaf1f0d2743b4efdfaa7b1fa08a
bayalams <110493296+bayalams@users.noreply.github.com>
2023-10-02 21:08:31 +0000

    Mon, Oct 2, 2023, 10:08 PM +01:00

--- lines/lines.py
+++ lines/lines.py
@@ -3,11 +3,12 @@ import sys
 
 path = sys.argv[1]
 
-while True:
-    try:
-        with open(path, "r") as file:
-            lines = file.readlines()
-    except FileNotFoundError:
-        print("File not found.")
-        sys.exit(1)
+try:
+    with open(path, "r") as file:
+        lines = file.readlines()
+except FileNotFoundError:
+    print("File not found.")
+    sys.exit(1)
 
+print(len(lines))
+if 
