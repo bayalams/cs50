cfabfeffa67efa673c7e63e4904fac96aa13b954
bayalams <110493296+bayalams@users.noreply.github.com>
2023-10-02 21:05:38 +0000

    Mon, Oct 2, 2023, 10:05 PM +01:00

--- lines/lines.py
+++ lines/lines.py
@@ -1,12 +1,13 @@
 import os
 import sys
 
-#path = sys.argv[1]
+path = sys.argv[1]
 
 while True:
     try:
-        with open("interpreter.py", "r") as file:
+        with open(path, "r") as file:
             lines = file.readlines()
-    except FileNotFound:
+    except FileNotFoundError:
+        print("File not found.")
         sys.exit(1)
 
