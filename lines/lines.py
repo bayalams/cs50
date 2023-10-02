f4270ba4a82a88035a33b370ae0a4596102dc24a
bayalams <110493296+bayalams@users.noreply.github.com>
2023-10-02 20:50:00 +0000

    Mon, Oct 2, 2023, 9:50 PM +01:00

--- lines/lines.py
+++ lines/lines.py
@@ -1,8 +1,8 @@
 import os
 
-path = /workspaces/110493296/interpreter/interpreter.py
+path = "/workspaces/110493296/interpreter/interpreter.py"
 
-with open("path", "r") as file:
+with open(path, "r") as file:
     lines = file.readlines()
     print(lines)
 
