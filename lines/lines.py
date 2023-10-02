ba94fdef10ed5a0c450538f0da92f078a5eb2e4a
bayalams <110493296+bayalams@users.noreply.github.com>
2023-10-02 17:42:45 +0000

    Mon, Oct 2, 2023, 6:42 PM +01:00

--- lines/lines.py
+++ lines/lines.py
@@ -2,9 +2,13 @@ import sys
 import os
 
 
-file = sys.argv[1]
-print(file)
+directory = sys.argv[1]
+print(directory)
 
-for filename in os.listdir(file):
-    with open(file) as file:
+if not os.path.isdir(directory):
+    print("Not directory")
+    sys.exit(1)
+
+for filename in os.listdir(directory):
+    with open(directory) as file:
         file.readlines()
