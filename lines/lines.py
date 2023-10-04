21d9154d4d290a7e012d67c28f72e0321047ba61
bayalams <110493296+bayalams@users.noreply.github.com>
2023-10-04 16:59:56 +0000

    Wed, Oct 4, 2023, 5:59 PM +01:00

--- lines/lines.py
+++ lines/lines.py
@@ -21,7 +21,7 @@ for line in lines:
         filtered_doc.append(line)
 
 for line in lines:
-    line = line.split("\n")
-    print(lines)
+    line = line.split("/n")
+    print(len(lines))
 
 print(len(filtered_doc))
