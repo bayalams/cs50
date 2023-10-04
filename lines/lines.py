0938e9cfbd60e9902e90686926209817b73c825c
bayalams <110493296+bayalams@users.noreply.github.com>
2023-10-04 18:12:33 +0000

    Wed, Oct 4, 2023, 7:12 PM +01:00

--- lines/lines.py
+++ lines/lines.py
@@ -17,7 +17,9 @@ except FileNotFoundError:
 for line in lines:
     line = line.lstrip()
     if line != "":
+        #if the line is empty, nothing happens, it goes back to the beggining
         if not line.startswith("#"):
+            #if the line isn't empty, it checks if it starts with an #. If it doesn't, it appends to the new list
             filtered_doc.append(line)
 
 print(f"len = {len(filtered_doc)}")
