be6b63c0937d063f9257bd5c039e66a8f019a292
bayalams <110493296+bayalams@users.noreply.github.com>
2023-10-05 15:34:13 +0000

    Thu, Oct 5, 2023, 4:34 PM +01:00

--- pizza/pizza.py
+++ pizza/pizza.py
@@ -7,6 +7,8 @@ prices = []
 
 if len(sys.argv) > 2:
     print("Too many command-line arguments")
+elif len(sys.argv) == 0:
+    print("Too few command-line arguments")
     sys.exit(1)
 elif not menu.endswith("csv"):
     print("Not a cvs file")
@@ -15,6 +17,7 @@ elif not menu.endswith("csv"):
 with open(menu) as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
+        #must append to a list instead of just printing so that the date is stored before being formated 
         prices.append({"Regular Pizza": row["Regular Pizza"], "Small": row["Small"], "Large": row["Large"]})
 
 print(tabulate(prices))
