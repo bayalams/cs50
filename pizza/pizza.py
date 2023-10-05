12b9a26a2b7e32633153e2d6916f010d7c13009b
bayalams <110493296+bayalams@users.noreply.github.com>
2023-10-05 14:29:54 +0000

    Thu, Oct 5, 2023, 3:29 PM +01:00

--- pizza/pizza.py
+++ pizza/pizza.py
@@ -3,6 +3,9 @@ import sys
 menu = sys.argv[1]
 
 if len(sys.argv) > 2:
+    print("Too many command-line arguments")
     sys.exit(1)
 elif not menu.endswith("cvs"):
+    print("Not a CVS file")
     sys.exit(1)
+
