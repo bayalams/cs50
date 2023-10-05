f1db243bee139390b84ffba6b5c8fc46e9a15d6c
bayalams <110493296+bayalams@users.noreply.github.com>
2023-10-05 14:52:29 +0000

    Thu, Oct 5, 2023, 3:52 PM +01:00

--- pizza/pizza.py
+++ pizza/pizza.py
@@ -7,13 +7,13 @@ menu = sys.argv[1]
 if len(sys.argv) > 2:
     print("Too many command-line arguments")
     sys.exit(1)
-elif not menu.endswith("cvs"):
-    print("Not a CVS file")
+elif not menu.endswith("csv"):
+    print("Not a cvs file")
     sys.exit(1)
 
-with open("regular.csv") as csvfile:
+with open(menu) as csvfile:
     reader = csv.DictReader(csvfile)
-    for row in csvfile:
+    for row in reader:
         print(row["Small"], row["Large"])
 
 print(row)
