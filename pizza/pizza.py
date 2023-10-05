cff239aa12f341324d12dbf4ae557885e5129595
bayalams <110493296+bayalams@users.noreply.github.com>
2023-10-05 14:40:09 +0000

    Thu, Oct 5, 2023, 3:40 PM +01:00

--- pizza/pizza.py
+++ pizza/pizza.py
@@ -1,8 +1,8 @@
 import sys
 from tabulate import tabulate
+import csv
 
 menu = sys.argv[1]
-print(menu)
 
 if len(sys.argv) > 2:
     print("Too many command-line arguments")
@@ -10,5 +10,9 @@ if len(sys.argv) > 2:
 elif not menu.endswith("cvs"):
     print("Not a CVS file")
     sys.exit(1)
+
+with open("menu") as csvfile:
+    reader = csvdile.Dictreader
+
 
 #print(tabulate(menu))
