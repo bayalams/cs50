e1219eaa6b4c51619bc5c7fae60a5fc420b9e540
bayalams <110493296+bayalams@users.noreply.github.com>
2023-10-07 15:22:55 +0000

    Sat, Oct 7, 2023, 4:22 PM +01:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -2,9 +2,14 @@ import sys
 import csv
 
 names_before = sys.argv[1]
-#names_after = sys.argv[2]
+names_after = sys.argv[2]
 
 with open(names_before) as csvfile_1:
     reader = csv.DictReader(csvfile_1)
     for row in reader:
         print(row)
+
+
+with open(names_after, "w", ) as csvfile_2:
+    fieldnames = ["first", "last", "house"]
+    writer = csv.DictWriter(csvfile_2, fieldnames = fieldnames)
