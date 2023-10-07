5ea4d5769a45d65f082bad173cb28f2bfe827e68
bayalams <110493296+bayalams@users.noreply.github.com>
2023-10-07 16:01:04 +0000

    Sat, Oct 7, 2023, 5:01 PM +01:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -12,14 +12,13 @@ house_list=[]
 with open(names_before) as csvfile_1:
     for line in csvfile_1:
         row = line.strip().split(",")
+        print(row)
         last = row[0]
         first = row[1]
-        print(f"last name: {last} | first name: {first}")
+        # house = row[2]
+        # print(f"last name: {last} | first name: {first} | house: {house}")
 
 
-
-
-
-# with open(names_after, "w", ) as csvfile_2:
-#     fieldnames = ["first", "last", "house"]
-#     writer = csv.DictWriter(csvfile_2, fieldnames = fieldnames)
+with open(names_after, "w", ) as csvfile_2:
+    fieldnames = ["first", "last", "house"]
+    writer = csv.DictWriter(csvfile_2, fieldnames = fieldnames)
