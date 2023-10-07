fcb6412ca483b25b93907d10bff218f4955312f5
bayalams <110493296+bayalams@users.noreply.github.com>
2023-10-07 15:10:08 +0000

    Sat, Oct 7, 2023, 4:10 PM +01:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -2,8 +2,9 @@ import sys
 import csv
 
 names_before = sys.argv[1]
-names_after = sys.argv[2]
+#names_after = sys.argv[2]
 
 with open(names_before) as csvfile_1:
     reader = csv.DictReader(csvfile_1)
-    print(reader)
+    for row in reader:
+        print(row)
