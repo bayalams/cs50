a3a3abcc86bc6548da08a068a7d0bc67eca9bb17
bayalams <110493296+bayalams@users.noreply.github.com>
2023-10-08 15:45:23 +0000

    Sun, Oct 8, 2023, 4:45 PM +01:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -13,29 +13,26 @@ elif len(sys.argv) < 3:
 names_before = sys.argv[1]
 names_after = sys.argv[2]
 
-before_list = []
-first_list = []
-last_list = []
-house_list=[]
 
 #open the file and split the names into last and first
-with open(names_before, "r") as csvfile_1:
+with open(names_before, "r") as csvfile_1, open(names_after, "w") as csvfile_2:
     reader = csv.DictReader(csvfile_1)
+
+    #DEFINE HEADERS
+    fieldnames = ["first", "last", "house"]
+    #DEFINE WRITER USING SAID HEADERS
+    writer = csv.DictWriter(csvfile_2, fieldnames = fieldnames)
+    #WRITE HEADERS ON FILE
+    writer.writeheader()
+
+    #LOOP FOR SPLITING FIRST AND LAST NAME
     for row in reader:
         full_name = row["name"]
         house = row["house"]
         last, first = full_name.split(",")
-        first_list.append(first)
-        last_list.append(last)
 
-# pprint(f"last: {last_list}")
-# pprint(f"first: {first_list}")
-# print(type(last_list))
-
-with open(names_after, "w", ) as csvfile_2:
-    fieldnames = ["first", "last", "house"]
-    writer = csv.DictWriter(csvfile_2, fieldnames = fieldnames)
-    writer.writerow({"first": first, "last": last, "house": house})
+        #MODULE FOR WRITING THE ACTUAL INFORMATION UNDER THE HEADER; MUST BE INDENTED BECAUSE OTHERWISE THE FILE WILL BE CLOSED
+        writer.writerow({"first": first, "last": last, "house": house})
 
 
 
