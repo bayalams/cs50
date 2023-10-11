e51380dd0e803524bc796c0151e23be2a0fe92c3
bayalams <110493296+bayalams@users.noreply.github.com>
2023-10-11 16:55:18 +0000

    Wed, Oct 11, 2023, 5:55 PM +01:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -2,10 +2,10 @@ from PIL import Image
 import sys
 
 puppet = sys.argv[1]
-# puppet_with_shirt = sys.argv[2]
+#puppet_with_shirt = sys.argv[2]
 
 
-shirt = Image.open(puppet)
+shirt = Image.open("shirt.png")
 print(shirt.format, shirt.size, shirt.mode)
 
 
