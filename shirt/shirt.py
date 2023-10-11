c086e98e893b44d3d2a51c81a8329d88cc1a18a1
bayalams <110493296+bayalams@users.noreply.github.com>
2023-10-11 17:00:20 +0000

    Wed, Oct 11, 2023, 6:00 PM +01:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -8,13 +8,4 @@ puppet = sys.argv[1]
 shirt = Image.open("shirt.png")
 print(shirt.format, shirt.size, shirt.mode)
 
-for infile in sys.argv[1:]:
-    f, e = os.path.splitext(infile)
-    outfile = f + ".jpg"
-    if infile != outfile:
-        try:
-            with Image.open(infile) as im:
-                im.save(outfile)
-        except OSError:
-            print("cannot convert", infile)
-
+PIL.ImageOps.fit()
