2cb9a34dd0c10f95f076615609e0af285af28fa1
bayalams <110493296+bayalams@users.noreply.github.com>
2023-10-11 16:56:12 +0000

    Wed, Oct 11, 2023, 5:56 PM +01:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -1,5 +1,5 @@
 from PIL import Image
-import sys
+import sys, os
 
 puppet = sys.argv[1]
 #puppet_with_shirt = sys.argv[2]
@@ -8,4 +8,13 @@ puppet = sys.argv[1]
 shirt = Image.open("shirt.png")
 print(shirt.format, shirt.size, shirt.mode)
 
+for infile in sys.argv[1:]:
+    f, e = os.path.splitext(infile)
+    outfile = f + ".jpg"
+    if infile != outfile:
+        try:
+            with Image.open(infile) as im:
+                im.save(outfile)
+        except OSError:
+            print("cannot convert", infile)
 
