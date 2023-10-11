17b2d7ec74004735b952933b963a8ddf51e938c9
bayalams <110493296+bayalams@users.noreply.github.com>
2023-10-11 17:33:55 +0000

    Wed, Oct 11, 2023, 6:33 PM +01:00

--- scourgify/scourgify1.py
+++ scourgify/scourgify1.py
@@ -1,5 +1,3 @@
-#FALTA PASSAR NOS TESTES TODOS
-
 import sys
 import csv
 from pprint import pprint
@@ -11,34 +9,27 @@ elif len(sys.argv) < 3:
     print("Too few command-line arguments")
     sys.exit(1)
 
-
 names_before = sys.argv[1]
 names_after = sys.argv[2]
 
-
-#open the file and split the names into last and first
-
-
+# Open the file for writing
 with open(names_after, "w") as csvfile_2:
-    #DEFINE HEADERS
+    # Define headers
     fieldnames = ["first", "last", "house"]
-    #DEFINE WRITER USING SAID HEADERS
-    writer = csv.DictWriter(csvfile_2, fieldnames = fieldnames)
-    #WRITE HEADERS ON FILE
+    # Define writer using said headers
+    writer = csv.DictWriter(csvfile_2, fieldnames=fieldnames)
+    # Write headers on file
     writer.writeheader()
 
+    # Open the file for reading
     with open(names_before, "r") as csvfile_1:
         reader = csv.DictReader(csvfile_1)
 
-        #LOOP FOR SPLITING FIRST AND LAST NAME
+        # Loop for splitting first and last name
         for row in reader:
             full_name = row["name"]
             house = row["house"]
             last, first = full_name.split(",")
 
-        #MODULE FOR WRITING THE ACTUAL INFORMATION UNDER THE HEADER; MUST BE INDENTED BECAUSE OTHERWISE THE FILE WILL BE CLOSED
-        writer.writerow({"first": first, "last": last, "house": house})
-
-
-
-
+            # Write the actual information under the header
+            writer.writerow({"first": first.strip(), "last": last.strip(), "house": house})
