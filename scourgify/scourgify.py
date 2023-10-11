af01ef37500d79348a67582cfdd7a030d40cd92e
bayalams <110493296+bayalams@users.noreply.github.com>
2023-10-11 17:26:04 +0000

    Wed, Oct 11, 2023, 6:26 PM +01:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -17,21 +17,21 @@ names_after = sys.argv[2]
 
 
 #open the file and split the names into last and first
-with open(names_before, "r") as csvfile_1, open(names_after, "w") as csvfile_2:
+with open(names_before, "r") as csvfile_1:
     reader = csv.DictReader(csvfile_1)
+     #LOOP FOR SPLITING FIRST AND LAST NAME
+    for row in reader:
+        full_name = row["name"]
+        house = row["house"]
+        last, first = full_name.split(",")
 
+with open(names_after, "w") as csvfile_2:
     #DEFINE HEADERS
     fieldnames = ["first", "last", "house"]
     #DEFINE WRITER USING SAID HEADERS
     writer = csv.DictWriter(csvfile_2, fieldnames = fieldnames)
     #WRITE HEADERS ON FILE
     writer.writeheader()
-
-    #LOOP FOR SPLITING FIRST AND LAST NAME
-    for row in reader:
-        full_name = row["name"]
-        house = row["house"]
-        last, first = full_name.split(",")
 
         #MODULE FOR WRITING THE ACTUAL INFORMATION UNDER THE HEADER; MUST BE INDENTED BECAUSE OTHERWISE THE FILE WILL BE CLOSED
     writer.writerow({"first": first, "last": last, "house": house})
