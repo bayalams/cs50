e85413fa95fd4fd7834e08e28d53a01e2556b510
bayalams <110493296+bayalams@users.noreply.github.com>
2023-10-14 12:03:52 +0000

    Sat, Oct 14, 2023, 1:03 PM +01:00

--- numb3rs/numb3rs.py
+++ numb3rs/numb3rs.py
@@ -3,4 +3,13 @@ import re
 ip_address = input("IPv4 Address: ")
 print(ip_address)
 
-if re.search(r")
+if re.search(r"(.+)/.(.+)/.(.+)/.(.+)"):
+    print(re.group(1))
+    print(re.group(2))
+    print(re.group(3))
+    print(re.group(4))
+
+    # if re.group(1) and re.group(2) and re.group(3) and re.group(4) <= 255:
+    #     print("valid")
+    # else:
+    #     print("invalid")
