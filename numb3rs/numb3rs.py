a84d4417efe5fec2b6887796dafee89046270120
bayalams <110493296+bayalams@users.noreply.github.com>
2023-10-14 13:03:30 +0000

    Sat, Oct 14, 2023, 2:03 PM +01:00

--- numb3rs/numb3rs.py
+++ numb3rs/numb3rs.py
@@ -9,8 +9,7 @@ g2 = int(matches.group(2))
 g3 = int(matches.group(3))
 g4 = int(matches.group(4))
 
-if g1<= 255 :
-    print(g1)
+if g1<= 255 | g2<= 255 | g3<= 255 | g4<= 255:
     print("valid")
 else:
     print("Invalid")
