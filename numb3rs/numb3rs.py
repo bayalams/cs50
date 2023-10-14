194b5e6c69bae714216a53df5a6f39df29573c19
bayalams <110493296+bayalams@users.noreply.github.com>
2023-10-14 13:23:17 +0000

    Sat, Oct 14, 2023, 2:23 PM +01:00

--- numb3rs/numb3rs.py
+++ numb3rs/numb3rs.py
@@ -1,16 +1,19 @@
 import re
 
 def main():
+    #the purpose of this function is to see if the input fits the pattern returned from the function validate()
     ip_address = input("IPv4 Address: ")
+    #define the variable matches in the main function, using the return value from validate()
+    matches = validate(ip_address)
     if matches:
         print("True")
     else:
         print("False")
 
 def validate(ip_address):
+    #the only purpose of this function is to return the pattern
     pattern = r"^(25[0-5]|2[0-4]\d|[01]?\d\d?)\.(25[0-5]|2[0-4]\d|[01]?\d\d?)\.(25[0-5]|2[0-4]\d|[01]?\d\d?)\.(25[0-5]|2[0-4]\d|[01]?\d\d?)$"
-    matches = re.search(pattern, ip_address)
+    return re.search(pattern, ip_address)
 
 if __name__ == "__main__":
-    matches = validate(ip_address)
     main()
