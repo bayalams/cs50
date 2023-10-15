2c26c1a309f279a4db2c34917516f929684c8fd2
bayalams <110493296+bayalams@users.noreply.github.com>
2023-10-15 13:54:45 +0000

    Sun, Oct 15, 2023, 2:54 PM +01:00

--- watch/watch.py
+++ watch/watch.py
@@ -8,21 +8,18 @@ def main():
     if short_url:
         print(short_url)
     else:
-        print("none 2")
         return None
 
 def parse(iframe_string):
     #creates the pattern with which to compare the input, if the part exists within the input, it will match
     pattern = r"https?://(?:www\.)?youtube\.com/embed/([a-zA-Z0-9_-]+)"
     match = re.search(pattern, iframe_string)
-    print(f"m1: {match.group(1)}")
 
     #if it matches, returns the match formatted as follows, which will be used in the main function
     if match:
         video_id = match.group(1)
         return f"https://youtu.be/{video_id}"
     else:
-        print("none 1")
         return None
 
 if __name__ == "__main__":
