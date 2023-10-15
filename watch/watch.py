461cc4b4a38cdc23ec96f54cc14ca010b6771f9d
bayalams <110493296+bayalams@users.noreply.github.com>
2023-10-15 13:27:09 +0000

    Sun, Oct 15, 2023, 2:27 PM +01:00

---
+++ watch/watch.py
@@ -0,0 +1,25 @@
+import re
+
+def main():
+    iframe_string = input("HTML: ")
+    short_url = parse(iframe_string)
+    if short_url:
+        print(f"Shortened URL: {short_url}")
+    else:
+        print("Invalid iframe embed code or the embed code does not contain a recognizable YouTube URL.")
+        return None
+
+def parse(iframe_string):
+    pattern = r'https://www\.youtube\.com/embed/([a-zA-Z0-9_-]+)'
+    match = re.search(pattern, iframe_string)
+
+    if match:
+        video_id = match.group(1)
+        return f"https://youtu.be/{video_id}"
+    else:
+        return None
+
+if __name__ == "__main__":
+    main()
+
+
