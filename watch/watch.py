8be93c24c180c1395f7a11faac03c2ca5252e3fb
bayalams <110493296+bayalams@users.noreply.github.com>
2023-10-15 13:44:38 +0000

    Sun, Oct 15, 2023, 2:44 PM +01:00

--- watch/watch.py
+++ watch/watch.py
@@ -2,18 +2,20 @@ import re
 
 def main():
     iframe_string = input("HTML: ")
-    #takes the input and passes it through the function that takes only the url part
+    #takes the input and passes it through the function that takes only the url part; this
     short_url = parse(iframe_string)
     #if the short url exists, then it prints it
     if short_url:
-        print(f"Shortened URL: {short_url}")
+        print(short_url)
     else:
         return None
 
 def parse(iframe_string):
+    #creates the pattern with which to compare the input, if the part exists within the input, it will match
     pattern = r"https?://www\.youtube\.com/embed/([a-zA-Z0-9_-]+)"
     match = re.search(pattern, iframe_string)
 
+    #if it matches, returns the match formatted as follows, which will be used in the main function
     if match:
         video_id = match.group(1)
         return f"https://youtu.be/{video_id}"
