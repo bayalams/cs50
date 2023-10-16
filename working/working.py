af6d0b0645b1b07d8050d3a6a3ceb66f6ad17fa4
bayalams <110493296+bayalams@users.noreply.github.com>
2023-10-16 21:26:35 +0000

    Mon, Oct 16, 2023, 10:26 PM +01:00

--- working/working.py
+++ working/working.py
@@ -1,26 +1,35 @@
+#falta o 0 antes da hora se for só 1 dígito
+#falta partir em funções
+
 import re
 hours = input("Hours: ")
 
-hours_pattern = re.search(r"^(\d{1,2}:\d{2}\s(?:AM|PM))\sto\s(\d{1,2}:\d{2}\s(?:AM|PM))$", hours)
 
-if hours_pattern:
-    hours1, hours2 = hours_pattern.group(1), hours_pattern.group(2)
-    print(hours1, hours2)
-else:
-    pass
+def main():
+    print(convert(input("Hours: ")))
 
-hour1, minutes1, half1 = re.split(r":|\s", hours1)
-hour2, minutes2, half2 = re.split(r":|\s", hours2)
+def convert(s):
+    hours_pattern = re.search(r"^(\d{1,2}:\d{2}\s(?:AM|PM))\sto\s(\d{1,2}:\d{2}\s(?:AM|PM))$", hours)
 
-print(hour1, minutes1, half1)
+    if hours_pattern:
+        hours1, hours2 = hours_pattern.group(1), hours_pattern.group(2)
+        print(hours1, hours2)
+    else:
+        pass
 
-if half1 == "PM":
-    hour1 = int(hour1)
-    hour1 = int(hour1 + 12)
-elif half2 == "PM":
-    hour2 = int(hour2)
-    hour2 = int(hour2 + 12)
-else:
-    pass
+    hour1, minutes1, half1 = re.split(r":|\s", hours1)
+    hour2, minutes2, half2 = re.split(r":|\s", hours2)
 
-print(f"{hour1}:{minutes1} to {hour2}:{minutes2}")
+    print(hour1, minutes1, half1)
+
+    if half1 == "PM":
+        hour1 = int(hour1)
+        hour1 = int(hour1 + 12)
+    elif half2 == "PM":
+        hour2 = int(hour2)
+        hour2 = int(hour2 + 12)
+    else:
+        pass
+
+    print(f"{hour1}:{minutes1} to {hour2}:{minutes2}")
+    return f"{hour1}:{minutes1} to {hour2}:{minutes2}"
