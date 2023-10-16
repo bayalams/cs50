16e274f404d4286c815bae33596865f56fb23bb2
bayalams <110493296+bayalams@users.noreply.github.com>
2023-10-16 20:47:39 +0000

    Mon, Oct 16, 2023, 9:47 PM +01:00

--- working/working.py
+++ working/working.py
@@ -1,11 +1,16 @@
 import re
 hours = input("Hours: ")
 
-hours_pattern = re.search(r"^(\d{1,2}:\d{2}\s(?:AM|PM))\sto\s(\d{1,2}:\d{2}\s)(?:AM|PM))$", hours)
-print(hours_pattern.group(1))
+hours_pattern = re.search(r"^(\d{1,2}:\d{2}\s(?:AM|PM))\sto\s(\d{1,2}:\d{2}\s(?:AM|PM))$", hours)
 
 if hours_pattern:
     hours1, hours2 = hours_pattern.group(1), hours_pattern.group(2)
     print(hours1, hours2)
 else:
     pass
+
+hours1, minutes1 = hours1.split(":", " ")
+hours2, minutes2 = hours1.split(":", " ")
+
+print(hours1, minutes1)
+print(hours2, minutes2)
