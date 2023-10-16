24e2c309f9494070297041676d99985aa4bce147
bayalams <110493296+bayalams@users.noreply.github.com>
2023-10-16 17:33:28 +0000

    Mon, Oct 16, 2023, 6:33 PM +01:00

--- working/working.py
+++ working/working.py
@@ -4,4 +4,7 @@ hours = input("Hours: ")
 hours_pattern = re.search(r"^(\d+)(AM | PM)(\d+)(AM | PM)$", hours)
 
 if hours_pattern:
-    hours1, hours2 = hours_pattern.group(1), 
+    hours1, hours2 = hours_pattern.group(1), hours_pattern.group(3)
+    print(hours1, hours2)
+else:
+    pass
