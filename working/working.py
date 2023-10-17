8654bbb9e6f3702a16d06fbea09a882101ac2ef5
bayalams <110493296+bayalams@users.noreply.github.com>
2023-10-17 19:34:32 +0000

    Tue, Oct 17, 2023, 8:34 PM +01:00

--- working/working.py
+++ working/working.py
@@ -39,15 +39,15 @@ def convert(hours):
     if half1 == "PM" and int(hour1) != 12:
         hour1 = int(hour1) + 12
     elif half1 == "AM" and int(hour1) == 12:
-        hour1 = "00"
+        hour1 = 00
 
     if half2 == "PM" and int(hour2) != 12:
         hour2 = int(hour2) + 12
     elif half2 == "AM" and int(hour2) == 12:
-        hour2 = "00"
+        hour2 = 00
 
 
-    return f"{hour1}:{minutes1} to {hour2}:{minutes2}"
+    return f"{hour1.zfill(2)}:{minutes1} to {hour2.zfill(2)}:{minutes2}"
 
 
 if __name__ == "__main__":
