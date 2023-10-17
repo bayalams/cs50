050c148cd260de862a515ea6377eac8dc01a8c1d
bayalams <110493296+bayalams@users.noreply.github.com>
2023-10-17 18:47:42 +0000

    Tue, Oct 17, 2023, 7:47 PM +01:00

--- working/working.py
+++ working/working.py
@@ -7,14 +7,15 @@ def main():
     print(convert(hours))
 
 def convert(hours):
-    hours_pattern = re.search(r"^(\d{1,2}(?::\d{2})?\s(?:AM|PM))\sto\s(\d{1,2}(?::\d{2})?\s(?:AM|PM))$", hours)
+    hours_pattern = re.search(r"^(\d{1,2}(::\d{2})?\s(?:AM|PM))\sto\s(\d{1,2}(::\d{2})?\s(?:AM|PM))$", hours)
 
     if hours_pattern:
-        hours1, hours2 = hours_pattern.group(1), hours_pattern.group(2)
+        hours1, hours2 = hours_pattern.group(1), hours_pattern.group(3)
         print(hours1, hours2)
     else:
         pass
 
+    
     hour1, minutes1, half1 = re.split(r":|\s", hours1)
     hour2, minutes2, half2 = re.split(r":|\s", hours2)
 
