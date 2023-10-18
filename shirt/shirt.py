3b35ca9a8e1eeddb74a5b6f87336fe017a4a576d
bayalams <110493296+bayalams@users.noreply.github.com>
2023-10-18 20:20:29 +0000

    Wed, Oct 18, 2023, 9:20 PM +01:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -1,4 +1,4 @@
-from PIL import Image, Resampling
+from PIL import Image
 from PIL import Image
 from PIL import ImageOps
 import sys
@@ -11,4 +11,4 @@ puppet = sys.argv[1]
 shirt = Image.open("shirt.png")
 print(shirt.format, shirt.size, shirt.mode)
 
-img1 = ImageOps.fit(puppet, method=Resampling.BICUBIC, bleed=0.0, centering=(0.5, 0.5))
+img1 = ImageOps.fit(puppet, bleed=0.0, centering=(0.5, 0.5))
