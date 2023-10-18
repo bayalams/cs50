e71071f1bf289280a22d02e3d6cb80aa647b3949
bayalams <110493296+bayalams@users.noreply.github.com>
2023-10-18 20:28:35 +0000

    Wed, Oct 18, 2023, 9:28 PM +01:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -13,11 +13,13 @@ OVERLAY_SHIRT = 'shirt.png'
 #puppet_with_shirt = sys.argv[2]
 
 
-if __main__ == '__main__':
-    if not check_puppet(file):
-        print(f'invalid puppet {file}. exit.')
+if __name__ == '__main__':
+    if not check_puppet(puppet):
+        print(f'invalid puppet {puppet}. exit.')
         exit(-1)
 
     img_puppet = Image.open(puppet)
     img_shirt = Image.open(OVERLAY_SHIRT)
-    
+    img_puppet.paste(img_shirt)
+
+    img_puppet.save(f'{puppet.split(".")[0]}_out.jpg')
