29beacfa394f10d9d20e6cf3177ace3d9a89dae6
bayalams <110493296+bayalams@users.noreply.github.com>
2023-10-21 12:29:48 +0000

    Sat, Oct 21, 2023, 1:29 PM +01:00

--- working/test_working.py
+++ working/test_working.py
@@ -3,13 +3,19 @@ from working import find_pattern, convert
 
 def main():
     test_find_pattern()
-    test_convert()
+    #test_convert()
 
 def test_find_pattern():
     assert "9:00 AM to 5:00 PM" == True
     assert "9 AM to 5 PM" == True
+    with pytest.raises(ValueError):
+        "9 AM - 5 PM"
+    with pytest.raises(ValueError):
+        "10:7 AM - 5:1 PM"
+    with pytest.raises(ValueError):
+        "09:00 to 17:00"
 
-def test_convert():
+#def test_convert():
 
 if __name__ == "__main__":
     main()
