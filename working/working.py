cafd8e772c353a65790e5a24aa9c79a881296253
bayalams <110493296+bayalams@users.noreply.github.com>
2023-10-22 18:26:44 +0000

    Sun, Oct 22, 2023, 7:26 PM +01:00

--- working/working.py
+++ working/working.py
@@ -11,10 +11,11 @@ def split_hours(hours):
     hours_pattern = re.search(r"^(\d{1,2}(:\d{2})?\s(?:AM|PM))\sto\s(\d{1,2}(:\d{2})?\s(?:AM|PM))$", hours)
 
     #see if input matches required pattern and retrieve the grouped information
-    if hours_pattern:
-        hours1, hours2 = hours_pattern.group(1), hours_pattern.group(3)
-    else:
+    if not hours_pattern:
         raise ValueError
+
+    hours1, hours2 = hours_pattern.group(1), hours_pattern.group(3)
+
 
     #split the given hours in hour, minutes and AM/PM
     split_hours1 = re.split(r":|\s", hours1)
