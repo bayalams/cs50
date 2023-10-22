c60561c0b6f7fd76e8d89721aa62f1916b2c17a7
bayalams <110493296+bayalams@users.noreply.github.com>
2023-10-22 17:13:18 +0000

    Sun, Oct 22, 2023, 6:13 PM +01:00

--- working/test_working.py
+++ working/test_working.py
@@ -17,10 +17,19 @@ def test_split_hours(input_hours, expected_split1, expected_split2):
     assert split_hours1 == expected_split1
     assert split_hours2 == expected_split2
 
-def test_split_hours_invalid(hours):
-    hours = "5 AM - 4 PM"
-    with test_split_hours_invalid(hours):
-        raise ValueError
+@pytest.mark.parametrize("invalid_hours", [
+    "25:00 AM to 5:00 PM",
+    "12:60 AM to 5:00 PM",
+    "9:00 AM to 13:00 PM",
+    "9 AM - 5 PM"
+    "9 to 5",
+    "9:AM to 5:00 PM",
+])
+
+def test_split_hours_invalid(invalid_hours):
+    with pytest.raises(ValueError):
+        test_split_hours_invalid(invalid_hours)
+
 
 @pytest.mark.parametrize("split_hours1, split_hours2, expected_output", [
     (['9', '00', 'AM'], ['5', '00', 'PM'], "09:00 to 17:00"),
