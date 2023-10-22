ac80a6897750c9cf602946bd094ad46247efcf5f
bayalams <110493296+bayalams@users.noreply.github.com>
2023-10-22 17:00:31 +0000

    Sun, Oct 22, 2023, 6:00 PM +01:00

--- working/test_working.py
+++ working/test_working.py
@@ -26,10 +26,9 @@ def test_convert(split_hours1, split_hours2, expected_output):
     result = convert(split_hours1, split_hours2)
     assert result == expected_output
 
-@pytest.mark.parametrize("split_hours1, split_hours2, invalid_hours", [
-    (['9', '00', 'AM'], ['5', '00', 'PM'], "09:00 to 17:00"),
-    (['9', 'AM'], ['5', 'PM'], "09:00 to 17:00"),
-    (['12', '00', 'PM'], ['12', '00', 'AM'], "12:00 to 00:00")
+@pytest.mark.parametrize("split_hours1, split_hours2, [
+    (['9', '1', 'AM'], ['5', '76', 'PM']),
+    (['23', 'AM'], ['15', 'PM'])
 ])
 
 def test_convert_invalid_hours(invalid_hours):
